adfbf885bd0dcd9d9f4a0ee8efb4f57b1dd11cd8
nils <nils.r.winter@gmail.com>
2017-02-10 14:49:45 +0100

    intial commit
    add basic structure of data objects

--- loading.py
+++ loading.py
@@ -5,99 +5,131 @@ Loading Module
 # Copyright (c) PHOTON Development Team
 
 import numpy as np
+import pandas as pd
 
+class DataContainer:
 
-class Container(dict):
-    """Container object for data
-
-    Creates dictionary-like object that stores all input data.
-
-    """
-
-    features = {}
-    targets = {}
-    covariates = {}
-
-
-
-class DataFrame:
-    """Container object for data
-
-    Creates dictionary-like object that stores all input data.
-
-    """
     def __init__(self):
-        """Return a data frame object with features, targets and
-        covariates"""
+        self.features = FeaturesObject()
+        self.targets = TargetsObject()
+        self.covariates = CovariatesObject()
 
-        self.features = {}
-        self.targets = {}
-        self.covariates = {}
+    def add(self, file, input_type, name, use_as=''):
+        dataObject = self.select_use(use_as)
+
+        if input_type == 'numpy':
+            dataObject.add_numpy(file, name)
+
+        elif input_type == 'excel':
+            dataObject.load_excel(file)
+
+        elif input_type == 'csv':
+            dataObject.load_csv(file)
+
+        elif input_type == 'mat':
+            pass
+
+        elif input_type == 'hdf5':
+            pass
+
+        else:
+            raise ValueError('Wrong input_type variable. Options: '
+                             'None, "excel", "mat", "hdf5".')
 
 
-    def add(self, data, name, use_as='', ):
-        """Add data variable to data frame
-
-        Adds one data variable at a time (i.e. a
-        covariate, a target variable or a data array).
-
-        Currently only works with numpy arrays.
-
-        Specify how the input data should be used with use_as='',
-        i.e. 'features', 'targets', 'covariate'.
-
-        Parameters
-        ----------
-        data : numpy array
-            Numpy array with arbitrary dimensions
-        name: string
-            String specifying name of variable
-        use_as: {'features', 'targets', 'covariate'}
-            String specifying how to use input variable
-
-        Returns
-        -------
-        data : Container object
-            Data container as dictionary.
-        """
-        if use_as == 'features':
-            self.features[name] = data
-        elif use_as == 'targets':
-            self.targets[name] = data
-        elif use_as == 'covariate':
-            self.covariates[name] = data
+    def select_use(self, use_as):
+        if use_as.lower() == 'features':
+            return self.features
+        elif use_as.lower() == 'targets':
+            return self.targets
+        elif use_as.lower() == 'covariate':
+            return self.covariates
         else:
             raise ValueError('Wrong use_as variable. Options: '
                              '"features", "targets", "covariate".')
 
+    def summary(self):
+        self.features.summary()
+        self.targets.summary()
+        self.covariates.summary()
+
+### implement these as singleton patterns
+
+class BaseObject:
+
+    def __init__(self):
+        # Create data dictionary to store the pandas arrays
+        # Maybe change this so one doesn't need a dictionary
+        # just one variable would be enough, basically...
+        # Maybe just a list?
+        self.data = {}
+        self.class_name = None
+
+
+    def add_numpy(self, data_in, name):
+        # add numpy array to data dictionary
+        # check if a pandas dataframe already exists
+        self.data[name] = pd.DataFrame({name: data_in})
+
+
+    def load_excel(self, file, **options):
+        # Use pandas to read excel data
+
+        # TO DO
+        # pass all options for pandas read_excel functions
+
+        data_in = pd.read_excel(file)
+
+        # For more than one variable, pandas will create a dataframe
+        # Split dataframe and save each variable separately
+        for key in list(data_in):
+            self.data[key] = pd.DataFrame(data_in[key])
+
+    def load_csv(self, file, **options):
+        # Use pandas to read excel data
+
+        # TO DO
+        # pass all options for pandas read_excel functions
+
+        data_in = pd.read_csv(file)
+
+        # For more than one variable, pandas will create a dataframe
+        # Split dataframe and save each variable separately
+        for key in list(data_in):
+            self.data[key] = pd.DataFrame(data_in[key])
 
     def summary(self):
-        """Get variables of Data Frame"""
-        print('Features:\n')
-        for key in self.features:
-            print(key, 'with shape',
-                  self.features[key].shape)
-            print('Mean', np.mean(self.features[key]))
-        print('\n\nTargets:\n')
-        for key in self.targets:
-            print(key, 'with shape',
-                  self.targets[key].shape)
-            print('Mean', np.mean(self.targets[key]))
-        print('\n\nCovariates:\n')
-        for key in self.covariates:
-            print(key, 'with shape',
-                  self.covariates[key].shape)
-            print('Mean', np.mean(self.covariates[key]))
+        """Get variables of Data Container"""
+        print(self.class_name, ':\n')
+        for key in self.data:
+            print(self.data[key].describe())
+            print('\n')
+
+    def get_names(self):
+        return self.data.keys()
 
 
+class FeaturesObject(BaseObject):
+    def __init__(self):
+        BaseObject.__init__(self)
+        self.class_name = 'Features'
+
+    def add_numpy(self, data_in, name):
+        # add numpy array to data dictionary
+        # check if a pandas dataframe already exists
+        self.data[name] = pd.DataFrame(data_in)
 
 
+class TargetsObject(BaseObject):
+    def __init__(self):
+        BaseObject.__init__(self)
+        self.class_name = 'Targets'
 
 
-
-
-
-
+class CovariatesObject(BaseObject):
+    def __init__(self):
+        BaseObject.__init__(self)
+        self.class_name = 'Covariates'
 
 
 
