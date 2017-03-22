978577ede1d43cd12a4e8a6f1b92a48b0df7ec05
nils <nils.r.winter@gmail.com>
2017-03-22 16:52:00 +0100

    add NiiLoader

--- DataLoader/DataLoader.py
+++ DataLoader/DataLoader.py
@@ -1,5 +1,10 @@
-import matlab_io as mio
 import pandas as pd
+import nibabel as nib
+import os
+import numpy as np
+
+from DataLoader import MatlabLoader as mio
+
 
 #Todo: make sure that each class is returning an pandas DataFrame Object
 
@@ -25,3 +30,45 @@ class XlsxLoader(object):
 
     def __call__(self, filename, **kwargs):
         return pd.read_excel(filename)
+
+class NiiLoader(object):
+
+    def __call__(self, directory, **kwargs):
+        # get nifti filenames in specified directory
+        filenames = self.get_filenames(directory)
+
+        # iterate over and load every .nii file
+        # this requires one nifti per subject
+        data = []
+        for ind_sub in range(len(filenames)):
+            filename = os.path.join(directory, filenames[ind_sub])
+            img = nib.load(filename)
+            data += img.get_data()
+
+        # stack list elements to matrix
+        data = np.stack(data, axis=0)
+        data = np.reshape(data, (data.shape[0], data.shape[1] *
+                                 data.shape[2] * data.shape[3]))
+        # save as numpy array and load with memory map
+        np.save((directory + '/photon_data.npy'), data)
+        del data
+        data = np.load((directory + '/photon_data.npy'), mmap_mode='r')
+        return pd.DataFrame(data)
+
+
+    def get_filenames(self, directory):
+        # get rid of asterisk in directory
+        # os.path.split will take head and tail of path
+        # only use the head
+        filenames = []
+        directory, _ = os.path.split(directory)
+        for file in os.listdir(directory):
+            if file.endswith(".nii"):
+                filenames += file
+
+        # check if files have been found
+        if len(filenames) == 0:
+            raise ValueError('There are no .nii-files in the '
+                             'specified folder!')
+        else:
+            return filenames
