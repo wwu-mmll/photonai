125cb6d781c88243231a2a7a9f156625ee0662e8
Ramona Leenings <kontakt@ramonaleenings.com>
2017-04-13 16:51:44 +0200

    fixed the bug that occurred when there is more than one item in a hyperpipe that is stacked in a pipeline fusion.
    organized the example files

--- hpo_framework_demo.py
+++ hpo_framework_demo.py
@@ -9,18 +9,10 @@ from sklearn.model_selection import KFold
 
 # Load data
 data_object = DataContainer()
-# # load ENIGMA surface values
-# data_object += Features('EnigmaTestFiles/CorticalMeasuresENIGMA_SurfAvg.csv',
-#                         usecols=np.arange(1, 73), na_values='NA')
-# # initial shape of loaded data:
-# print('feature shape before concat', data_object.features.data.shape)
-# add values from another file, namely ENIGMA thickness values, to features
+
+# add values from ENIGMA thickness values to features
 data_object += Features('EnigmaTestFiles/CorticalMeasuresENIGMA_ThickAvg.csv',
                         usecols=np.arange(1, 73), na_values='NA')
-
-# when adding more than one data source to features or targets, the data is internally concatenated horizontally
-# --> see shape after concat -> Nils removed concat!
-# print('feature shape after concat', data_object.features.data.shape)
 
 # try to predict sex, which is column number 4
 data_object += Targets('EnigmaTestFiles//Covariates.csv', usecols=[4],
@@ -48,23 +40,27 @@ print(data_object.covariates['age'])
 # 01. pca
 # 02. keras neuronal net  OR support vector classifier OR logistic regression
 cv_object = KFold(n_splits=3)
+
 manager = Hyperpipe('god', cv_object)
 
 # add a pca analysis, specify hyperparameters to test
-manager += PipelineElement.create('pca', {'n_components': np.arange(10, 70, 10).tolist()}, set_disabled=True)
+manager += PipelineElement.create('pca', {'n_components': [None, 10, 20], 'whiten': [True, False]},
+                                  set_disabled=True)
+
+# ADD ANY KERAS MODEL LIKE SHOWN IN WRAPPERMODEL CLASS
+# manager += PipelineElement.create('wrapper_model', {'learning_rate': [0.1, 0.2, 0.3]})
 
 # add a neuronal net
 # add a neural network, hyperparameters = try out x hidden layers with several sizes, set default values
-# manager += PipelineElement('kdnn', {'hidden_layer_sizes': [[10], [5, 10], [10, 20, 10]]},
-#                            batch_normalization=True, learning_rate=0.3, target_dimension=10)
-
+# kdnn = PipelineElement.create('kdnn', {'hidden_layer_sizes': [[10, 5], [5]]},
+#                               batch_normalization=True, learning_rate=0.3, target_dimension=10)
 
 # you can also use a SVC
 svc_estimator = PipelineElement.create('svc', {'C': np.arange(0.2, 1, 0.2), 'kernel': ['rbf', 'sigmoid']})
 # or Logistic regression
 lr_estimator = PipelineElement.create('logistic', {'C': np.logspace(-4, 4, 5)})
 
-manager += PipelineSwitch('final_estimator', [svc_estimator, lr_estimator])
+manager.add(PipelineSwitch('final_estimator', [svc_estimator, lr_estimator]))
 
 # or whatever you want...
 # the syntax is always: PipelineElement(Element identifier, hyperparameter dictionary, options to pass to the element)
@@ -73,3 +69,12 @@ manager += PipelineSwitch('final_estimator', [svc_estimator, lr_estimator])
 X = data_object.features.values
 y = np.ravel(data_object.targets.values)
 manager.fit(X, y)
+
+final_predictor = manager.optimum_pipe
+final_predictor.fit(X, y)
+prediction = final_predictor.predict(X)
+
+# access the performance and config histories
+config1 = manager.config_history[0]
+performance1 = manager.performance_history[0]
+
