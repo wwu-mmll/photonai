40f995208d4f7370f1b56d087dc3690d05da4819
Ramona Leenings <kontakt@ramonaleenings.com>
2017-05-31 15:15:12 +0200

    - fix major difference between Photon and manual implementation in TestCaseA2 by making sure a standard_scaler is present in both - edit the Wrapper Model class so that setting of a new parameter through the hyperpipe is printed to the console

--- PipelineWrapper/WrapperModel.py
+++ PipelineWrapper/WrapperModel.py
@@ -3,18 +3,23 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 
 class WrapperModel(BaseEstimator, ClassifierMixin):
 
-    def __init__(self, learning_rate, model):
+    def __init__(self, learning_rate=1, model=None):
         self.learning_rate = learning_rate
         self.model = model
 
     def fit(self, data, targets):
         # whatever needs to be done ....
         # do anything with self.learning_rate
-        self.model.fit(data, targets)
+        # self.model.fit(data, targets)
         return self
 
     def predict(self, data):
         return self.model.predict(data)
+
+    def set_params(self, **params):
+        print('learning_rate before:', self.learning_rate)
+        super(WrapperModel, self).set_params(**params)
+        print('learning_rate after:', self.learning_rate)
 
 
 
