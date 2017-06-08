35736bf34f1155ef107b535f29fe4dd1d4ef1739
Ramona Leenings <kontakt@ramonaleenings.com>
2017-06-08 14:57:05 +0200

    adding infrastructure for hyperparameter testing

--- PipelineWrapper/RLCNN.py
+++ PipelineWrapper/RLCNN.py
@@ -5,54 +5,59 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 
 class RLCNN(BaseEstimator, ClassifierMixin):
 
-    def __init__(self, learning_rate=1, model=None):
+    def __init__(self, image_width=24*12, image_height=5*60, num_labels=5, learning_rate=1e-4,
+                 number_densely_neurons=1024, patch_size=100, num_features=32, reduction_1=6, reduction_2=2):
+
+        self.image_width = image_width
+        self.image_height = image_height
+        self.num_labels = num_labels
+
         self.learning_rate = learning_rate
-        self.model = model
+
+        self.number_densely_neurons = number_densely_neurons
+        self.patch_size = patch_size
+        self.num_features = num_features
+        self.reduction_1 = reduction_1
+        self.reduction_2 = reduction_2
+
+        self.x = None
+        self.y_ = None
+        self.y_conv = None
 
     def fit(self, data, targets):
 
-        image_width = 24*12
-        image_height = 5*60
-
-        num_labels = 5
-        densely_neurons = 1024
-
-        patch_size = 10
-        num_features = 32
-        reduction_1 = 6
-        reduction_2 = 2
-
-        self.x = tf.placeholder(tf.float32, [None, image_width*image_height])
+        self.x = tf.placeholder(tf.float32, [None, self.image_width*self.image_height])
         # 1st dimension: number of data entries
         # 2nd and 3rd dimension: image width and height
         # 4th dimension: number of color channels.
-        x_image = tf.reshape(self.x, [-1, image_width, image_height, 1])
+        x_image = tf.reshape(self.x, [-1, self.image_width, self.image_height, 1])
 
         # labels
-        self.y_ = tf.placeholder(tf.float32, [None, num_labels])
+        self.y_ = tf.placeholder(tf.float32, [None, self.num_labels])
 
         # first convolution
         # compute 32 features for each 5x5 patch.
-        W_conv1 = RLCNN.weight_variable([patch_size, patch_size, 1, num_features])
-        b_conv1 = RLCNN.bias_variable([num_features])
+        W_conv1 = RLCNN.weight_variable([self.patch_size, self.patch_size, 1, self.num_features])
+        b_conv1 = RLCNN.bias_variable([self.num_features])
         # filter & remove negative numbers
         h_conv1 = tf.nn.relu(RLCNN.conv2d(x_image, W_conv1) + b_conv1)
         # max_pool_2x2 => reduce from 28x28 to 14x14.
-        h_pool1 = RLCNN.max_pool_2x2(h_conv1, reduction_1)
+        h_pool1 = RLCNN.max_pool_2x2(h_conv1, self.reduction_1)
 
         # second convolution
         # compute 64 features for each 5x5 patch.
-        W_conv2 = RLCNN.weight_variable([patch_size, patch_size, num_features, 2*num_features])
-        b_conv2 = RLCNN.bias_variable([2*num_features])
+        W_conv2 = RLCNN.weight_variable([self.patch_size, self.patch_size, self.num_features, 2*self.num_features])
+        b_conv2 = RLCNN.bias_variable([2*self.num_features])
         h_conv2 = tf.nn.relu(RLCNN.conv2d(h_pool1, W_conv2) + b_conv2)
         # reduce from 14x14 to 7x7
-        h_pool2 = RLCNN.max_pool_2x2(h_conv2, reduction_2)
+        h_pool2 = RLCNN.max_pool_2x2(h_conv2, self.reduction_2)
 
         # densely connected layer
         # weights = 7x7 image and 64 features, 1024 neurons
-        final_size = (image_width/reduction_1/reduction_2) * (image_height/reduction_1/reduction_2) * 2 * num_features
-        W_fc1 = RLCNN.weight_variable([final_size, densely_neurons])
-        b_fc1 = RLCNN.bias_variable([densely_neurons])
+        final_size = int((self.image_width/self.reduction_1/self.reduction_2) *
+                         (self.image_height/self.reduction_1/self.reduction_2) * 2 * self.num_features)
+        W_fc1 = RLCNN.weight_variable([final_size, self.number_densely_neurons])
+        b_fc1 = RLCNN.bias_variable([self.number_densely_neurons])
 
         h_pool2_flat = tf.reshape(h_pool2, [-1, final_size])
         h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
@@ -62,8 +67,8 @@ class RLCNN(BaseEstimator, ClassifierMixin):
         h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
         # final decision layer
-        W_fc2 = RLCNN.weight_variable([densely_neurons, num_labels])
-        b_fc2 = RLCNN.bias_variable([densely_neurons])
+        W_fc2 = RLCNN.weight_variable([self.number_densely_neurons, self.num_labels])
+        b_fc2 = RLCNN.bias_variable([self.number_densely_neurons])
 
         self.y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
@@ -82,13 +87,12 @@ class RLCNN(BaseEstimator, ClassifierMixin):
         sess = tf.InteractiveSession()
         sess.run(tf.global_variables_initializer())
 
-        for i in range(10):
+        for i in range(100):
             train_accuracy = accuracy.eval(feed_dict={self.x: data, self.y_: targets, keep_prob: 1.0})
             print("step %d, training accuracy %g" % (i, train_accuracy))
 
             # train
             train_step.run(feed_dict={self.x: data, self.y_: targets, keep_prob: 0.5})
-
 
         print("test accuracy %g" % accuracy.eval(feed_dict={
             self.x: data, self.y_: targets, keep_prob: 1.0}))
@@ -99,8 +103,9 @@ class RLCNN(BaseEstimator, ClassifierMixin):
     #     return data
 
     def predict(self, data):
-        return self.model.y_conv(feed_dict={
+        classification = tf.run(self.y_conv, feed_dict={
             self.x: data})
+        return classification
 
     @staticmethod
     def weight_variable(shape):
