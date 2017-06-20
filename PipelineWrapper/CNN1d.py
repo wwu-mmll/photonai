b2ec839473d24f170522fe42408069af9b5a1023
nils <nils.r.winter@gmail.com>
2017-06-20 10:51:29 +0200

    add create_model() and define_optimizer() for CNN1d

--- PipelineWrapper/CNN1d.py
+++ PipelineWrapper/CNN1d.py
@@ -2,8 +2,8 @@ import numpy as np
 import tensorflow as tf
 import keras
 from keras.models import Sequential
-from keras.layers import Dropout, Dense
-from keras.optimizers import Adam
+from keras.layers import Dropout, Dense, Conv1D, MaxPooling1D, Flatten
+import keras.optimizers
 from keras.layers.core import Activation
 from keras.layers.normalization import BatchNormalization
 from keras.layers.advanced_activations import PReLU
@@ -12,139 +12,113 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 
 class CNN1d(BaseEstimator, ClassifierMixin):
 
-    def __init__(self, image_width=24*12, image_height=5*60, num_labels=5, learning_rate=1e-4,
-                 number_densely_neurons=1024, patch_size=100, num_features=32, reduction_1=6, reduction_2=2):
+    def __init__(self, target_dimension, n_filters,
+                           kernel_size, n_convolutions_per_block,
+                           pooling_size,
+                           stride, size_last_layer,
+                           act_func='relu', learning_rate=0.001,
+                           dropout_rate=0, batch_normalization=True,
+                           nb_epochs=200, batch_size=64,
+                           loss='categorical_crossentropy',
+                           metrics=['accuracy'], optimizer='adam', gpu_device='/gpu:0'):
 
-        self.image_width = image_width
-        self.image_height = image_height
-        self.num_labels = num_labels
-
-        self.learning_rate = learning_rate
-
-        self.number_densely_neurons = number_densely_neurons
-        self.patch_size = patch_size
-        self.num_features = num_features
-        self.reduction_1 = reduction_1
-        self.reduction_2 = reduction_2
+        self.target_dimension = target_dimension
+        self.n_filters = n_filters
+        self.kernel_size = kernel_size
+        self.n_convolutions_per_block = n_convolutions_per_block
+        self.pooling_size = pooling_size
+        self.stride = stride
+        self.size_last_layer = size_last_layer
+        self.act_func = act_func
+        self.lr = learning_rate
+        self.dropout = dropout_rate
+        self.batch_normalization = batch_normalization
+        self.nb_epochs = nb_epochs
+        self.loss = loss
+        self.metrics = metrics
+        self.optimizer = optimizer
+        self.batch_size = batch_size
+        self.gpu_device = gpu_device
 
         self.x = None
         self.y_ = None
-        self.y_conv = None
+        self.model = None
 
-    def create_model(input_shape=[], n_classes=[], n_filters=[],
-                           kernel_sizes=[], n_convolutions_per_block=[],
-                           pooling_size=[],
-                           strides=[], size_last_layer=[],
-                           actFunc='relu', learning_rate=0.001,
-                           dropout_rate=0, batch_normalization=False,
-                           nb_epoch=200,
-                           loss='categorical_crossentropy',
-                           metrics=['accuracy'], optimizer='adam',
-                           gpu_device='/gpu:0'):
+    def fit(self, X, y):
+        if self.target_dimension > 1:
+            y = self.dense_to_one_hot(y, self.target_dimension)
 
-        # size_last_layer
-        # n_filters
-        # kernel_size
-        # strides
-        # n_convolutions_per_block
+        self.model = self.create_model(X.shape)
+        self.model.fit(X,y, batch_size=self.batch_size, epochs=self.nb_epochs, verbose=0)
 
-        ks = kernel_sizes
-        ps = pooling_size
-        nf = n_filters
-        input_shape = (input_shape[0], input_shape[1], input_shape[2], 1)
 
+    def create_model(self, input_shape):
         model = Sequential()
 
-        for ind_blocks in range(len(n_filters)):
-            for ind_convs in range(n_convolutions_per_block):
+        for ind_blocks in range(len(self.n_filters)):
+            for ind_convs in range(self.n_convolutions_per_block):
                 if ind_blocks == 0 and ind_convs == 0:
-                    with tf.device(gpu_device):
-                        model.add(Conv3D(nf[ind_blocks],
-                                         [ks[0], ks[1], ks[2]],
-                                         strides=strides,
+                    with tf.device(self.gpu_device):
+                        model.add(Conv1D(self.n_filters[ind_blocks],
+                                         self.kernel_size,
+                                         strides=self.stride,
                                          padding='same',
                                          input_shape=input_shape))
-                        model.add(Activation(actFunc))
-                    if batch_normalization:
+                        model.add(Activation(self.act_func))
+                    if self.batch_normalization:
                         model.add(BatchNormalization())
                 else:
-                    with tf.device(gpu_device):
-                        model.add(Conv3D(nf[ind_blocks],
-                                         [ks[0], ks[1], ks[2]],
-                                         strides=strides,
+                    with tf.device(self.gpu_device):
+                        model.add(Conv1D(self.n_filters[ind_blocks],
+                                         self.kernel_size,
+                                         strides=self.stride,
                                          padding='same'))
-                        model.add(Activation(actFunc))
+                        model.add(Activation(self.act_func))
 
-                    if batch_normalization:
+                    if self.batch_normalization:
                         model.add(BatchNormalization())
-            with tf.device(gpu_device):
-                if ps:
-                    model.add(MaxPooling3D(pool_size=(ps, ps, ps)))
+            with tf.device(self.gpu_device):
+                if self.pooling_size:
+                    model.add(MaxPooling1D(pool_size=self.pooling_size))
 
-                if dropout_rate:
-                    model.add(Dropout(dropout_rate))
+                if self.dropout:
+                    model.add(Dropout(self.dropout))
 
-        with tf.device(gpu_device):
+        with tf.device(self.gpu_device):
             model.add(Flatten())
-            model.add(Dense(size_last_layer))
-            model.add(Activation(actFunc))
-            if dropout_rate:
-                model.add(Dropout(dropout_rate))
-        if batch_normalization:
+            model.add(Dense(self.size_last_layer))
+            model.add(Activation(self.act_func))
+            if self.dropout:
+                model.add(Dropout(self.dropout))
+        if self.batch_normalization:
             model.add(BatchNormalization())
-        with tf.device(gpu_device):
-            model.add(Dense(n_classes))
+        with tf.device(self.gpu_device):
+            model.add(Dense(self.target_dimension))
             model.add(Activation('softmax'))
 
         # initiate RMSprop optimizer
-        optimizer = define_optimizer(optimizer_type=optimizer,
-                                     lr=learning_rate)
+        optimizer = self.define_optimizer(optimizer_type=self.optimizer,
+                                     lr=self.lr)
 
         # Let's train the model using RMSprop
-        model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
-        print(model.summary())
+        model.compile(loss=self.loss, optimizer=optimizer, metrics=self.metrics)
         return model
 
-    def define_optimizer(optimizer_type='adam', lr=0.001):
-        # TO DO: - use *kwargs to allow specification of additional optimizer hyperparameters
-        #        - learn optimizer, see DeepMind paper
-        #        - enable tf custom optimizer use
-        if optimizer_type.lower() == 'adam':
-            optimizer = keras.optimizers.Adam(lr=lr, beta_1=0.9,
-                                              beta_2=0.999,
-                                              epsilon=1e-08, decay=0.0)
-
-        elif optimizer_type.lower() == 'sgd':
-            optimizer = keras.optimizers.SGD(lr=lr, momentum=0.0,
-                                             decay=0.0, nesterov=False)
-
-        elif optimizer_type.lower() == 'rmsprop':
-            optimizer = keras.optimizers.RMSprop(lr=lr, rho=0.9,
-                                                 epsilon=1e-08,
-                                                 decay=0.0)
-
-        elif optimizer_type.lower() == 'adagrad':
-            optimizer = keras.optimizers.Adagrad(lr=lr, epsilon=1e-08,
-                                                 decay=0.0)
-
-        elif optimizer_type.lower() == 'adadelta':
-            optimizer = keras.optimizers.Adadelta(lr=lr, rho=0.95,
-                                                  epsilon=1e-08,
-                                                  decay=0.0)
-
-        elif optimizer_type.lower() == 'adamax':
-            optimizer = keras.optimizers.Adamax(lr=lr, beta_1=0.9,
-                                                beta_2=0.999,
-                                                epsilon=1e-08,
-                                                decay=0.0)
-
-        elif optimizer_type.lower() == 'nadam':
-            optimizer = keras.optimizers.Nadam(lr=lr, beta_1=0.9,
-                                               beta_2=0.999,
-                                               epsilon=1e-08,
-                                               schedule_decay=0.004)
-
-        else:
-            raise ValueError('Choose a valid keras optimizer!')
+    def define_optimizer(self, optimizer_type='Adam', lr=0.001):
+        # Todo: use kwargs to allow for additional optimizer tweaking
+        try:
+            optimizer_class = getattr(keras.optimizers, optimizer_type)
+            optimizer = optimizer_class(lr=lr)
+        except AttributeError as ae:
+            raise ValueError('Could not find optimizer:',
+                             optimizer_type, ' - check spelling!')
 
         return optimizer
+
+    def dense_to_one_hot(self, labels_dense, num_classes):
+        """Convert class labels from scalars to one-hot vectors."""
+        num_labels = labels_dense.shape[0]
+        index_offset = np.arange(num_labels) * num_classes
+        labels_one_hot = np.zeros((num_labels, num_classes))
+        labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+        return labels_one_hot
