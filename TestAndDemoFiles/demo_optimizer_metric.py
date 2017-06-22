758f458cfee02157ad715e7b9fa6f5490879de7d
nils <nils.r.winter@gmail.com>
2017-06-22 15:00:57 +0200

    add demo_optimizer_metric

---
+++ TestAndDemoFiles/demo_optimizer_metric.py
@@ -0,0 +1,51 @@
+import unittest
+import numpy as np
+from sklearn.model_selection import KFold
+from HPOFramework.HPOBaseClasses import PipelineElement, Hyperpipe
+from sklearn.model_selection._validation import _fit_and_score
+import random
+
+from sklearn.datasets import load_breast_cancer
+dataset = load_breast_cancer()
+X = dataset.data
+y = dataset.target
+random.seed(42)
+
+pca_n_components = 10
+svc_c = 1
+svc_kernel = "rbf"
+# SET UP HYPERPIPE and choose accuracy as optimizer metric
+my_pipe = Hyperpipe('primary_pipe', optimizer='grid_search', optimizer_params={},
+                    metrics=['accuracy', 'precision', 'f1_score'],
+                    hyperparameter_specific_config_cv_object=KFold(n_splits=3),
+                    hyperparameter_search_cv_object=KFold(n_splits=3),
+                    eval_final_performance = True,
+                    optimizer_metric='accuracy')
+
+my_pipe += PipelineElement.create('standard_scaler')
+my_pipe += PipelineElement.create('pca', {'n_components': [pca_n_components]})
+my_pipe += PipelineElement.create('svc', {'C': [svc_c], 'kernel': [svc_kernel]})
+
+# START HYPERPARAMETER SEARCH
+print('-----------------')
+print('OPTIMIZER METRIC: ACCURACY\n\n\n')
+my_pipe.fit(X, y)
+print(my_pipe.test_performances)
+
+# SET UP HYPERPIPE and choose precision as optimizer metric
+my_pipe = Hyperpipe('primary_pipe', optimizer='grid_search', optimizer_params={},
+                    metrics=['accuracy', 'precision', 'f1_score'],
+                    hyperparameter_specific_config_cv_object=KFold(n_splits=3),
+                    hyperparameter_search_cv_object=KFold(n_splits=3),
+                    eval_final_performance = True,
+                    optimizer_metric='precision')
+
+my_pipe += PipelineElement.create('standard_scaler')
+my_pipe += PipelineElement.create('pca', {'n_components': [pca_n_components]})
+my_pipe += PipelineElement.create('svc', {'C': [svc_c], 'kernel': [svc_kernel]})
+
+# START HYPERPARAMETER SEARCH
+print('\n\n\n-----------------')
+print('OPTIMIZER METRIC: PRECISION\n\n\n')
+my_pipe.fit(X, y)
+print(my_pipe.test_performances)
