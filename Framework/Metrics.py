d27d4a7969d56b879892801331ada148e53fc9ca
nils <nils.r.winter@gmail.com>
2017-08-17 11:56:57 +0200

    rename categorical_accuracy to categorical_accuracy_score

--- Framework/Metrics.py
+++ Framework/Metrics.py
@@ -5,7 +5,7 @@ Define custom metrics here
 import keras.backend as K
 
 
-def categorical_accuracy(y_true, y_pred):
+def categorical_accuracy_score(y_true, y_pred):
     return K.cast(K.equal(K.argmax(K.variable(y_true), axis=-1),
                           K.argmax(K.variable(y_pred), axis=-1)),
                   K.floatx())
