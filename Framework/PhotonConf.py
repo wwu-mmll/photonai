52810a20c949020433d13e4ebf3137cf1126400d
Claas Kaehler <claas.kaehler@uni-muenster.de>
2017-09-04 23:22:15 +0200

    Added a configfile (especially for the logger)

--- Framework/PhotonConf.py
+++ Framework/PhotonConf.py
@@ -2,7 +2,7 @@ import configparser
 from pathlib import Path
 
 class PhotonConf:
-    configfile_name = "configuration.ini"
+    configfile_path = "../configuration.ini"
 
 
     def createDefaultConfig(self):
@@ -11,21 +11,24 @@ class PhotonConf:
             'print_to_file': True,
             'logfile_name' : 'photon_out.log',
             'print_to_slack': False,
-            'slack_token': {},
+            'slack_token': 'enter token here!',
             'loglevel_slack': 'INFO'
         }
-        with open(self.configfile_name, 'w') as configfile:
+        with open(self.configfile_path, 'w') as configfile:
             self.config.write(configfile)
 
     def __init__(self):
-
         self.config = configparser.ConfigParser()
-        configfile = Path(self.configfile_name)
+        configfile = Path(self.configfile_path)
         if configfile.is_file():
             # file exists
-            self.config.read(self.configfile_name)
+            print("Loading configuration file.")
+            self.config.read(self.configfile_path)
         else:
-            print("Creating configuration file")
+            print("Creating configuration file.")
             self.createDefaultConfig()
 
 
+## Tests
+configtest = PhotonConf()
+print(configtest.config['LOGGING']['logfile_name'])
