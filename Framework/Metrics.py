dc0676c65fcbabbeef054330b082dabe5ca68fe5
Claas Kaehler <claas.kaehler@wwu.de>
2017-09-11 20:06:13 +0200

    Add metrics for regression:  - Pearson Correlation  - explained Variance

--- Framework/Metrics.py
+++ Framework/Metrics.py
@@ -15,3 +15,9 @@ def categorical_accuracy_score(y_true, y_pred):
         y_pred = one_hot_to_binary(y_pred)
 
     return accuracy_score(y_true, y_pred)
+
+def pearson_correlation(y_true, y_pred):
+    return np.corrcoef(y_true, y_pred)[0][1]
+
+def variance_explained(y_true, y_pred):
+    return np.square(pearson_correlation(y_true, y_pred))
