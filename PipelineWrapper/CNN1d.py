298fdcd29154821eb04fc8e29357ac6e5a8f49ae
nils <nils.r.winter@gmail.com>
2017-09-15 11:44:06 +0200

    Add callback functionality to CNN1d wrapper

--- PipelineWrapper/CNN1d.py
+++ PipelineWrapper/CNN1d.py
@@ -6,22 +6,25 @@ from keras.layers import Dropout, Dense, Conv1D, MaxPooling1D, Flatten, Gaussian
 from keras.layers.core import Activation
 from keras.layers.normalization import BatchNormalization
 from keras.models import Sequential
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from sklearn.base import BaseEstimator, ClassifierMixin
-
+from sklearn.model_selection import ShuffleSplit
 
 class CNN1d(BaseEstimator, ClassifierMixin):
     # todo: BUGFIX --> pooling doesnt work
-    def __init__(self, target_dimension=2, n_filters=[16], kernel_size=3,
+    def __init__(self, target_dimension=2, filter_per_block=[16], kernel_size=3,
                  pooling_size=3, stride=5, size_last_layer=10,
                  n_convolutions_per_block=1, gaussian_noise=0,
                  act_func='relu', learning_rate=0.001,
                  dropout_rate=0, batch_normalization=True,
                  nb_epochs=200, batch_size=64,
                  loss='categorical_crossentropy', metrics=['accuracy'],
-                 optimizer='adam', gpu_device='/gpu:0'):
+                 optimizer='adam', gpu_device='/gpu:0',
+                 early_stopping_flag=True, eaSt_patience=20,
+                 reLe_factor=0.4, reLe_patience=5):
 
         self.target_dimension = target_dimension
-        self.n_filters = n_filters
+        self.filter_per_block = filter_per_block
         self.kernel_size = kernel_size
         self.n_convolutions_per_block = n_convolutions_per_block
         self.pooling_size = pooling_size
@@ -38,6 +41,10 @@ class CNN1d(BaseEstimator, ClassifierMixin):
         self.batch_size = batch_size
         self.gpu_device = gpu_device
         self.gaussian_noise = gaussian_noise
+        self.early_stopping_flag = early_stopping_flag
+        self.eaSt_patience = eaSt_patience
+        self.reLe_factor = reLe_factor
+        self.reLe_patience = reLe_patience
 
         self.x = None
         self.y_ = None
@@ -48,9 +55,55 @@ class CNN1d(BaseEstimator, ClassifierMixin):
             y = self.dense_to_one_hot(y, self.target_dimension)
 
         self.model = self.create_model(X.shape)
-        self.model.fit(X,y, batch_size=self.batch_size, epochs=self.nb_epochs, verbose=0)
+
+        # Reshape X to add dimension for CNN (RGB channel)
+        X = np.reshape(X, (X.shape[0], X.shape[1], 1))
+
+        # use callbacks only when size of training set is above 100
+        if X.shape[-1] > 100:
+            # get pseudo validation set for keras callbacks
+            splitter = ShuffleSplit(n_splits=1, test_size=0.2)
+            for train_index, val_index in splitter.split(X):
+                X_train = X[train_index]
+                X_val = X[val_index]
+                y_train = y[train_index]
+                y_val = y[val_index]
+
+            # register callbacks
+            callbacks_list = []
+            # use early stopping (to save time;
+            # does not improve performance as checkpoint will find the best model anyway)
+            if self.early_stopping_flag:
+                early_stopping = EarlyStopping(monitor='val_loss',
+                                               patience=self.eaSt_patience)
+                callbacks_list += [early_stopping]
+
+            # adjust learning rate when not improving for patience epochs
+            reduce_lr = ReduceLROnPlateau(monitor='val_loss',
+                                          factor=self.reLe_factor,
+                                          patience=self.reLe_patience,
+                                          min_lr=0.001, verbose=0)
+            callbacks_list += [reduce_lr]
+
+            # fit the model
+            results = self.model.fit(X_train, y_train,
+                                     validation_data=(X_val, y_val),
+                                     batch_size=128,
+                                     epochs=self.nb_epoch,
+                                     verbose=0,
+                                     callbacks=callbacks_list)
+        else:
+            # fit the model
+            print(
+                'Cannot use Keras Callbacks because of small sample size...')
+            results = self.model.fit(X, y, batch_size=128,
+                                     epochs=self.nb_epoch,
+                                     verbose=0)
+
+        return self
 
     def predict(self, X):
+        X = np.reshape(X, (X.shape[0], X.shape[1], 1))
         if self.target_dimension > 1:
             predict_result = self.model.predict(X, batch_size=self.batch_size)
             max_index = np.argmax(predict_result, axis=1)
@@ -61,12 +114,11 @@ class CNN1d(BaseEstimator, ClassifierMixin):
     def create_model(self, input_shape):
         model = Sequential()
         input_shape = (input_shape[1], input_shape[2])
-        for ind_blocks in range(len(self.n_filters)):
+        for ind_blocks in range(len(self.filter_per_block)):
             for ind_convs in range(self.n_convolutions_per_block):
                 if ind_blocks == 0 and ind_convs == 0:
                     with tf.device(self.gpu_device):
-                        print(input_shape)
-                        model.add(Conv1D(self.n_filters[ind_blocks],
+                        model.add(Conv1D(self.filter_per_block[ind_blocks],
                                          self.kernel_size,
                                          strides=self.stride,
                                          padding='same',
@@ -76,7 +128,7 @@ class CNN1d(BaseEstimator, ClassifierMixin):
                         model.add(BatchNormalization())
                 else:
                     with tf.device(self.gpu_device):
-                        model.add(Conv1D(self.n_filters[ind_blocks],
+                        model.add(Conv1D(self.filter_per_block[ind_blocks],
                                          self.kernel_size,
                                          strides=self.stride,
                                          padding='same'))
