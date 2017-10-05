b3f6b2901b1660ffb0810d2cff713dec86f58ef7
nils <nils.r.winter@gmail.com>
2017-10-05 17:52:57 +0200

    Add predict_proba function NN wrappers

--- PipelineWrapper/SiameseDNNClassifier.py
+++ PipelineWrapper/SiameseDNNClassifier.py
@@ -124,6 +124,9 @@ class SiameseDNNClassifier(BaseEstimator, ClassifierMixin):
         max_index = np.argmax(predict_result, axis=1)
         return self.dense_to_one_hot(max_index, self.target_dimension)
 
+    def predict_proba(self, X):
+        return self.model.predict(X, batch_size=128)
+
     def score(self, X, y_true):
         return np.zeros(1)
 
