0e073e700c719aaf6b8f4912508eb908119da4c2
Claas Kähler <claas.kaehler@wwu.de>
2017-11-08 16:12:07 +0100

    Code was clean until manager requested to fuck it up

--- DemoFiles/Jhonny_dti.py
+++ DemoFiles/Jhonny_dti.py
@@ -19,7 +19,8 @@ def load_etc_subject_ids_and_targets(xls_file_path: str):
     # subject IDs
     subject_ids = pandas.read_excel(open(xls_file_path, 'rb'), sheetname='ECT', parse_cols="A", squeeze=True)
     # load ECT targets
-    targets = pandas.read_excel(open(xls_file_path, 'rb'), sheetname='ECT', parse_cols="K", squeeze=True)
+    targets = pandas.read_excel(open(xls_file_path, 'rb'), sheetname='ECT', parse_cols="I", squeeze=True)
+    targets = targets.as_matrix()
     Logger().debug(targets)
     return (subject_ids, targets)
 
@@ -54,18 +55,18 @@ def extract_unique_dti_features(unstripped_data: np.array):
     Logger().info("Element wise equal columns: {0}, {1} will be left.".format(len(columns_to_delete), unstripped_data.shape[1] - len(columns_to_delete)))
     return np.delete(unstripped_data, np.asarray(columns_to_delete), 1)
 
-subject_ids, targets = load_etc_subject_ids_and_targets(ROOT_DIR + '/Key_Information_ECT_sample_20170829.xlsx')
-
-cached_preprocessed_dti_data_file = Path(ROOT_DIR + "/cached_preprocessed_dti_data.npy")
-if cached_preprocessed_dti_data_file.exists():
-    Logger().info("Loading cached preprocessed DTI data...")
-    dti_roi_brain_striped = np.load(cached_preprocessed_dti_data_file)
-else:
-    dti_image_files = load_dti_images(ROOT_DIR + '/ECT_data_for_Tim/MD/', subject_ids)
-    dti_roi_brain = extract_brain_from_dti_images(dti_image_files)
-    dti_roi_brain_striped = extract_unique_dti_features(dti_roi_brain)
-    dti_roi_brain_striped = np.nan_to_num(dti_roi_brain_striped)
-    np.save(cached_preprocessed_dti_data_file, dti_roi_brain_striped)
+def load_and_preprocess_dti_data():
+    cached_preprocessed_dti_data_file = Path(ROOT_DIR + "/cached_preprocessed_dti_data.npy")
+    if cached_preprocessed_dti_data_file.exists():
+        Logger().info("Loading cached preprocessed DTI data...")
+        dti_roi_brain_striped = np.load(cached_preprocessed_dti_data_file)
+    else:
+        dti_image_files = load_dti_images(ROOT_DIR + '/ECT_data_for_Tim/MD/', subject_ids)
+        dti_roi_brain = extract_brain_from_dti_images(dti_image_files)
+        dti_roi_brain_striped = extract_unique_dti_features(dti_roi_brain)
+        dti_roi_brain_striped = np.nan_to_num(dti_roi_brain_striped)
+        np.save(cached_preprocessed_dti_data_file, dti_roi_brain_striped)
+    return dti_roi_brain_striped
 
 def classical_classification() -> Hyperpipe:
     # Strategie 1 - klassisch:
@@ -81,26 +82,35 @@ def classical_classification() -> Hyperpipe:
     #    - GPC
     #    - naive Bayes
     pipe = Hyperpipe('primary_pipe', optimizer='timeboxed_random_grid_search',
-                optimizer_params={"limit_in_minutes": 0.1},
-                metrics=['accuracy'],
+                optimizer_params={"limit_in_minutes": 2},
+                metrics=['mean_squared_error'],
                 inner_cv=LeaveOneOut(),
                 outer_cv=LeaveOneOut(),
                 eval_final_performance=True)
+    # pipe = Hyperpipe('primary_pipe', optimizer='timeboxed_random_grid_search',
+    #             optimizer_params={"limit_in_minutes": 0.05},
+    #             metrics=['mean_squared_error', 'pearson_correlation'],
+    #             inner_cv=KFold(n_splits=3, shuffle=True),
+    #             outer_cv=KFold(n_splits=3, shuffle=True),
+    #             eval_final_performance=True)
     standard_scaler = PipelineElement.create("standard_scaler", hyperparameters={}, test_disabled=True)
     #min_max_scaler = PipelineElement.create("standard_scaler", hyperparameters={}, test_disabled=True)
     pipe += standard_scaler
     pipe += PipelineElement.create("pca", hyperparameters={}, test_disabled=True, n_components=None)
-    pipe += PipelineElement.create("f_classif_select_percentile", hyperparameters={"percentile": [10, 50]}, test_disabled=True)
-    svc = PipelineElement.create("svc", hyperparameters={"kernel":["rbf", "linear"], "C":[0.5, 1, 2]})
-    dtree = PipelineElement.create("svc", hyperparameters={"kernel":["rbf", "linear"], "C":[0.5, 1, 2]})
-    pipe += svc
+    pipe += PipelineElement.create("f_regression_select_percentile", hyperparameters={"percentile": [10, 50]}, test_disabled=True)
+
+    #svr = PipelineElement.create("SVR", hyperparameters={"kernel":["rbf", "linear"], "C":[0.5, 1, 2]})
+    rndf = PipelineElement.create("RandomForestRegressor", hyperparameters={"min_samples_leaf": [1, 5], })
+    pipe += rndf
 
     return pipe
 
+subject_ids, targets = load_etc_subject_ids_and_targets(ROOT_DIR + '/Key_Information_ECT_sample_20170829.xlsx')
+dti_preprocessed = load_and_preprocess_dti_data()
 pipe = classical_classification()
 
-pipe.fit(np.ascontiguousarray(dti_roi_brain_striped),targets.as_matrix())
+pipe.fit(np.ascontiguousarray(dti_preprocessed),targets)
 
-logex = LogExtractor(pipe.results)
-logex.etract_csv('jhonny_dti_results.csv')
+logex = LogExtractor.LogExtractor(pipe.result_tree)
+logex.extract_csv('johnny_dti_results.csv')
 
