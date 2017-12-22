cc677cf212852c9529693309d791e6a82121310c
TimHahn <TimHahn14@gmail.com>
2017-12-22 10:39:30 +0100

    added permutation test for importance scores both for uncorrected and ROI-wise corrected p-values

--- DemoFiles/BAGDR/perm_test.py
+++ DemoFiles/BAGDR/perm_test.py
@@ -3,10 +3,45 @@ import numpy as np
 
 def perm_test_importance(n_perms, importance_score_file):
     # get true vals (no permutation)
-    true_imps = pandas.read_pickle(path=importance_score_file)
-    p_imp = true_imps.copy()
+    true_imps_raw = pandas.read_pickle(path=importance_score_file)
+    true_imps = pandas.DataFrame(index=true_imps_raw.index.values, columns=true_imps_raw.columns.values)
+    # get mean over folds
+    for col_id in true_imps_raw.columns.values:
+        for row_id in true_imps_raw.index.values:
+            true_imps.loc[row_id][col_id] = np.mean(true_imps_raw.loc[row_id][col_id])
 
-    return p_imp
+    # get perm vals and p-values for two-tailed, uncorrected test
+    imp_p = pandas.DataFrame(index=true_imps.index.values, columns=true_imps.columns.values)
+    for row_id in imp_p.index.values:
+        for col_id in imp_p.columns.values:
+            perms = list()
+            for permInd in range(1, n_perms + 1):
+                # get mean over folds for all permutations
+                a = pandas.read_pickle(path=importance_score_file + '_perm_' + str(permInd))
+                perms.append(np.mean(a.loc[row_id][col_id]))
+            # get p-values for two-tailed, uncorrected test
+            p = np.sum(np.abs(true_imps.loc[row_id][col_id]) <= np.abs(perms)) / len(perms)
+            if p == 0:
+                p = 1 / len(perms)
+            imp_p.loc[row_id][col_id] = p
+
+    # get perm vals and p-values for two-tailed, ROI-wise CORRECTED test
+    imp_p_cor = pandas.DataFrame(index=true_imps.index.values, columns=true_imps.columns.values)
+    for row_id in imp_p_cor.index.values:
+        perms = list()
+        for col_id in imp_p_cor.columns.values:
+            for permInd in range(1, n_perms + 1):
+                # get mean over folds for all permutations
+                a = pandas.read_pickle(path=importance_score_file + '_perm_' + str(permInd))
+                perms.append(np.mean(a.loc[row_id][col_id]))
+        for col_id in imp_p_cor.columns.values:
+            # get p-values for two-tailed, ROI-wise CORRECTED test
+            p = np.sum(np.abs(true_imps.loc[row_id][col_id]) <= np.abs(perms)) / len(perms)
+            if p == 0:
+                p = 1 / len(perms)
+            imp_p_cor.loc[row_id][col_id] = p
+
+    return imp_p, imp_p_cor
 
     # get permutations
 def perm_test_multCompCor(n_perms, results_file, metrics):
@@ -25,10 +60,10 @@ def perm_test_multCompCor(n_perms, results_file, metrics):
         perm_vec = np.asarray([item for sublist in perms for item in sublist])
         perm_vec = np.nan_to_num(perm_vec)
 
-        # get p-values
+        # get p-values for two-tailed test
         met_true = true_vals[metric]
         for i in p_vals.index:
-            p = np.sum(met_true[i] <= perm_vec) / len(perm_vec)
+            p = np.sum(np.abs(met_true[i]) <= np.abs(perm_vec)) / len(perm_vec)
             if p==0:
                 p = 1 / len(perm_vec)
             p_vals.loc[i, metric] = p
