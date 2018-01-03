0c9f3baa2c22f2d19a30bab0dcff8a8d894a7a79
Ramona Leenings <kontakt@ramonaleenings.com>
2018-01-03 14:19:54 +0100

    delete DemoFiles Folder

--- DemoFiles/MultiTask/show_results.py
+++
@@ -1,8 +0,0 @@
-
-import pandas
-#pre = 'C:/Users/hahnt/myGoogleDrive/work/Papers/_underConstruction/BrainAtlasOfGeneticDepressionRisk/data_now_on_Titania/'
-pre = '/spm-data/Scratch/spielwiese_tim/BrainAtlasOfGeneticDepressionRisk/'
-#pre = 'D:/myGoogleDrive/work/Papers/_underConstruction/BrainAtlasOfGeneticDepressionRisk/'
-test = pandas.read_pickle(path=pre + 'Results/metrics_summary_test_oneHot')
-
-print('')
