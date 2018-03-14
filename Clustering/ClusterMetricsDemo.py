5d6692921a3161521f24d6a09c2fb2835482fd5d
Ramona Leenings <kontakt@ramonaleenings.com>
2018-03-14 10:26:36 +0100

    add BIC

--- Clustering/ClusterMetricsDemo.py
+++ Clustering/ClusterMetricsDemo.py
@@ -1,10 +1,10 @@
 
-from Clustering.ClusterMetrics import ClusterMetrics
+from Clustering.ClusterPerformance import ClusterPerformance
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
+from sklearn.preprocessing import StandardScaler
 
-
-cluster_metric_helper = ClusterMetrics(KMeans, [2, 3, 4, 5, 6], plot=True)
+cluster_metric_helper = ClusterPerformance(KMeans, [2, 3, 4, 5, 6], plot=True)
 
 X, y = make_blobs(n_samples=500,
                   n_features=2,
@@ -14,4 +14,5 @@ X, y = make_blobs(n_samples=500,
                   shuffle=True,
                   random_state=1)  # For reproducibility
 
-cluster_metric_helper.fit(X)
+
+cluster_metric_helper.fit(StandardScaler().fit_transform(X))
