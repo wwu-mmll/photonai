ad4b6a0fb868a5321d35545b6c378e08111aaad5
Ramona Leenings <kontakt@ramonaleenings.com>
2018-04-12 14:11:21 +0200

    add some more pydoc strings and tidy up code

--- PhotonNeuro/Register_Photon_Neuro_comps.py
+++ PhotonNeuro/Register_Photon_Neuro_comps.py
@@ -9,19 +9,19 @@ element_type = 'Transformer'
 RegisterPipelineElement(photon_name=photon_name,
                         photon_package=package_name,
                         class_str=class_str,
-                        element_type=element_type).add()
+                        element_type=element_type).save()
 
 photon_name = 'SmoothImgs'
 class_str = 'PhotonNeuro.ImageBasics.SmoothImgs'
 RegisterPipelineElement(photon_name=photon_name,
                         photon_package=package_name,
                         class_str=class_str,
-                        element_type=element_type).add()
+                        element_type=element_type).save()
 
 photon_name = 'BrainAtlas'
 class_str = 'PhotonNeuro.BrainAtlas'
 RegisterPipelineElement(photon_name=photon_name,
                         photon_package=package_name,
                         class_str=class_str,
-                        element_type=element_type).add()
+                        element_type=element_type).save()
 
