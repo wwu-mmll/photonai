83ee9dc36decd042c86b04459537624384e3c04f
Nils <nils.r.winter@gmail.com>
2018-05-25 16:42:57 +0200

    Change project structure so that we can put PHOTON on PyPI

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'photon-ai',
+  packages = ['photon-ai'], # this must be the same as the name above
+  version = 'alpha0.1',
+  description = 'A Python-Based Hyperparameter Optimization Toolbox for Neural Networks',
+  author = 'PHOTON Team',
+  author_email = 'hahnt@wwu.de',
+  url = 'https://github.com/photon-team/photon.git', # use the URL to the github repo
+  download_url = 'https://github.com/photon-team/photon/archive/alpha0.1.tar.gz', # I'll explain this in a second
+  keywords = ['machine learning', 'deep learning', 'neural networks', 'hyperparameter'], # arbitrary keywords
+  classifiers = [],
+)
