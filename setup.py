abdd04c86b8e6d79957fb790df50d202c5b39b03
Nils <nils.r.winter@gmail.com>
2018-05-25 17:58:49 +0200

    Change version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'photon-ai',
   packages = ['photon-ai'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'A Python-Based Hyperparameter Optimization Toolbox for Neural Networks',
   author = 'PHOTON Team',
   author_email = 'hahnt@wwu.de',
   url = 'https://github.com/photon-ai-team/photon-ai.git', # use the URL to the github repo
-  download_url = 'https://github.com/photon-ai-team/photon-ai/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/photon-ai-team/photon-ai/archive/0.2.tar.gz', # I'll explain this in a second
   keywords = ['machine learning', 'deep learning', 'neural networks', 'hyperparameter'], # arbitrary keywords
   classifiers = [],
   install_requires = [
