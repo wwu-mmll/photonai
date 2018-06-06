c7a282dca59a19d12e5a65e51eea96fac2619042
Ramona Leenings <kontakt@ramonaleenings.com>
2018-06-06 18:07:37 +0200

    add callback functionality to inner cv loop for breaking a configuration test

--- photonai/examples/basic_pipeline.py
+++ photonai/examples/basic_pipeline.py
@@ -1,9 +1,9 @@
 
 from photonai.base.PhotonBase import Hyperpipe, PipelineElement
 from photonai.optimization.Hyperparameters import FloatRange, Categorical
+from photonai.optimization.SpeedHacks import MinimumPerformance
 
 from sklearn.model_selection import KFold
-
 from sklearn.datasets import load_breast_cancer
 X, y = load_breast_cancer(True)
 
@@ -14,8 +14,9 @@ my_pipe = Hyperpipe('basic_svm_pipe',
                     outer_cv=KFold(n_splits=3),
                     inner_cv=KFold(n_splits=10),
                     calculate_metrics_across_folds=True,
-                    eval_final_performance=False)
-
+                    eval_final_performance=False,
+                    inner_cv_callback_function=MinimumPerformance('accuracy', 0.96),
+                    verbosity=1)
 
 my_pipe += PipelineElement('StandardScaler')
 my_pipe += PipelineElement('PCA', {'n_components': [5, 10, None]})
