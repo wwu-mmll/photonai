45054a2cba5e27a627908aa0ee7aad4feb98406a
Nils <nils.r.winter@gmail.com>
2018-06-08 09:35:45 +0200

    Add find_packages() to setup file

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
   name = 'photonai',
-  packages = ['photonai', 'photonai.base', 'photonai.configuration', 'photonai.documentation', 'photonai.examples',
-              'photonai.genetics', 'photonai.helpers', 'photonai.investigator', 'photonai.modelwrapper', 'photonai.neuro',
-              'photonai.optimization', 'photonai.photonlogger', 'photonai.sidepackages', 'photonai.test', 'photonai.validation'],
-  version = '0.3.6',
+  packages = [find_packages()],
+      # 'photonai', 'photonai.base', 'photonai.configuration', 'photonai.documentation', 'photonai.examples',
+      #         'photonai.genetics', 'photonai.helpers', 'photonai.investigator', 'photonai.modelwrapper', 'photonai.neuro',
+      #         'photonai.optimization', 'photonai.photonlogger', 'photonai.sidepackages', 'photonai.test', 'photonai.validation'],
+  version = '0.3.7',
   description = 'A Python-Based Hyperparameter optimization Toolbox for Neural Networks',
   author = 'PHOTON Team',
   author_email = 'hahnt@wwu.de',
   url = 'https://github.com/photonai-team/photonai.git', # use the URL to the github repo
-  download_url = 'https://github.com/photonai-team/photonai/archive/0.3.6.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/photonai-team/photonai/archive/0.3.7.tar.gz', # I'll explain this in a second
   keywords = ['machine learning', 'deep learning', 'neural networks', 'hyperparameter'], # arbitrary keywords
   classifiers = [],
   install_requires = [
