64084d6e9ff0cdc496248b243878005d4d54f0b5
Nils <nils.r.winter@gmail.com>
2018-06-08 09:18:48 +0200

    Change version to 0.3.5

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
   name = 'photonai',
-  packages = ['photonai'], # this must be the same as the name above
-  version = '0.3.3',
+  packages = ['photonai'],
+  version = '0.3.5',
   description = 'A Python-Based Hyperparameter optimization Toolbox for Neural Networks',
   author = 'PHOTON Team',
   author_email = 'hahnt@wwu.de',
   url = 'https://github.com/photonai-team/photonai.git', # use the URL to the github repo
-  download_url = 'https://github.com/photonai-team/photonai/archive/0.3.3.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/photonai-team/photonai/archive/0.3.5.tar.gz', # I'll explain this in a second
   keywords = ['machine learning', 'deep learning', 'neural networks', 'hyperparameter'], # arbitrary keywords
   classifiers = [],
   install_requires = [
