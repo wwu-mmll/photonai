a03b0fadce9ee152bfc88c1bd3e40fe5432d6ce6
nils <nils.r.winter@gmail.com>
2018-06-21 04:49:48 +0200

    Update version

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
   name = 'photonai',
   packages = find_packages(),
   include_package_data=True,
-  version = '0.1.5',
+  version = '0.1.6',
   description = 'A Python-Based Hyperparameter optimization Toolbox for Neural Networks',
   author = 'PHOTON Team',
   author_email = 'hahnt@wwu.de',
   url = 'https://github.com/photonai-team/photonai.git', # use the URL to the github repo
-  download_url = 'https://github.com/photonai-team/photonai/archive/0.1.5.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/photonai-team/photonai/archive/0.1.6.tar.gz', # I'll explain this in a second
   keywords = ['machine learning', 'deep learning', 'neural networks', 'hyperparameter'], # arbitrary keywords
   classifiers = [],
   install_requires = [
