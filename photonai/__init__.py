5e0cc492ed223c2ffc52b316dc2b5cc7f0927d1a
Nils <nils.r.winter@gmail.com>
2018-07-10 08:14:07 +0200

    Update version

--- photonai/__init__.py
+++ photonai/__init__.py
@@ -23,7 +23,7 @@ from .investigator import Investigator
 
 from . import configuration
 from .configuration.Register import PhotonRegister
-__version__ = "0.1.9"
+__version__ = "0.2.0"
 
 
 __all__ = ("base",
