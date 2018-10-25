61c448c7ef15fbcfa76f297aca596b014b7b908b
Nils <nils.r.winter@gmail.com>
2018-10-25 19:05:48 +0200

    Merge freesurfer_age branch into beta1 and change version number

--- photonai/__init__.py
+++ photonai/__init__.py
@@ -23,7 +23,7 @@ from .investigator import Investigator
 
 from . import configuration
 from .configuration.Register import PhotonRegister
-__version__ = "0.3.6"
+__version__ = "0.3.7"
 
 
 __all__ = ("base",
