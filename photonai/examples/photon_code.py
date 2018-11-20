03c323a532ca00b90e0f696bfd8be51a2d5ed8b2
Ramona Leenings <kontakt@ramonaleenings.com>
2018-11-20 15:59:34 +0100

    fully implemented skOpt Optimizer for Photon

--- photonai/examples/photon_code.py
+++ photonai/examples/photon_code.py
@@ -7,7 +7,7 @@ import numpy as np
 from sklearn.model_selection import KFold, ShuffleSplit
 
 from photonai.base.PhotonBase import Hyperpipe, PipelineElement, PersistOptions
-from photonai.optimization.Hyperparameters import Categorical, FloatRange
+from photonai.optimization.Hyperparameters import Categorical, FloatRange, IntegerRange
 
 # Load data
 df = pd.read_excel('/spm-data/Scratch/photon_wizard/rleenings/examples/boston_housing/boston_data.xlsx')
@@ -44,13 +44,13 @@ hyperpipe = Hyperpipe('Boston Housing',
 
 # Add transformer elements
 hyperpipe += PipelineElement("StandardScaler", {'with_mean': [True], 'with_std': [True]}, test_disabled=True)
-hyperpipe += PipelineElement("PCA", {'n_components': [None, 10], 'random_state': [15], 'whiten': [False]},
+hyperpipe += PipelineElement("PCA", {'n_components': IntegerRange(3, 13), 'random_state': [15], 'whiten': [False]},
                              test_disabled=False)
 # Add estimator
-hyperpipe += PipelineElement("SVR", {'C': [0.5, 1.0, 1.5, 2.0],
+hyperpipe += PipelineElement("SVR", {'C': FloatRange(0.5, 2.0),
                                      'epsilon': [0.1],
                                      'gamma': [],
-                                     'kernel': ['linear', 'rbf']})
+                                     'kernel': Categorical(['linear', 'rbf'])})
 
 # Fit hyperpipe
 hyperpipe.fit(X, y)
--- photonai/optimization/SkOpt.py
+++ photonai/optimization/SkOpt.py
@@ -6,17 +6,21 @@ from skopt.space import Real, Integer
 from skopt.space import Categorical as skoptCategorical
 import numpy as np
 
+
 class SkOptOptimizer(PhotonBaseOptimizer):
 
-    def __init__(self, metric_to_optimize='accuracy', num_iterations: int=20):
+    def __init__(self, num_iterations: int=20):
         self.optimizer = None
         self.hyperparameter_list = []
-        self.metric_to_optimize = metric_to_optimize
+        self.metric_to_optimize = ''
         self.ask = self.ask_generator()
         self.num_iterations = num_iterations
+        self.maximize_metric = True
 
-    def prepare(self, pipeline_elements: list):
+    def prepare(self, pipeline_elements: list, maximize_metric: bool):
 
+        self.hyperparameter_list = []
+        self.maximize_metric = maximize_metric
         # build space
         space = []
         for pipe_element in pipeline_elements:
@@ -25,6 +29,7 @@ class SkOptOptimizer(PhotonBaseOptimizer):
                 if skopt_param is not None:
                     space.append(skopt_param)
         self.optimizer = Optimizer(space, "ET")
+        self.ask = self.ask_generator()
 
     def _convert_PHOTON_to_skopt_space(self, hyperparam: object, name: str):
         if not hyperparam:
@@ -42,11 +47,21 @@ class SkOptOptimizer(PhotonBaseOptimizer):
     def ask_generator(self):
         for i in range(self.num_iterations):
             next_config_list = self.optimizer.ask()
-            next_config_dict = {self.hyperparameter_list[number]: value for number, value in enumerate(next_config_list)}
+            next_config_dict = {self.hyperparameter_list[number]: self._convert_to_native(value) for number, value in enumerate(next_config_list)}
             yield next_config_dict
+
+    def _convert_to_native(self, obj):
+        # check if we have a numpy object, if so convert it to python native
+        if type(obj).__module__ == np.__name__:
+            return np.asscalar(obj)
+        else:
+            return obj
 
     def tell(self, config, performance):
         # convert dictionary to list in correct order
         config_values = [config[name] for name in self.hyperparameter_list]
-        random_accuracy = np.random.randn(1)[0]
-        self.optimizer.tell(config_values, random_accuracy)
+        best_config_metric_performance = performance[1]
+        if self.maximize_metric:
+            best_config_metric_performance = -best_config_metric_performance
+        # random_accuracy = np.random.randn(1)[0]
+        self.optimizer.tell(config_values, best_config_metric_performance)
