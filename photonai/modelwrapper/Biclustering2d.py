8dc567f4665c101ce071d5c48a383b69119c394e
LeonardGustavo <v_hols01@uni-muenster.de>
2019-04-30 13:47:45 +0200

    Repaired Biclustering Wrapper (tested), Added SamplePairing (by Nils Winter)

--- photonai/modelwrapper/Biclustering2d.py
+++ photonai/modelwrapper/Biclustering2d.py
@@ -2,12 +2,13 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.cluster import SpectralBiclustering
 import numpy as np
 import os
+from matplotlib import pyplot as plt
 #from photonai.photonlogger.Logger import Logger
 
 class Biclustering2d(BaseEstimator, TransformerMixin):
     _estimator_type = "transformer"
 
-    def __init__(self, n_clusters, random_state=42, scale='bistochastic', n_components=6,
+    def __init__(self, n_clusters=4, random_state=42, scale='bistochastic', n_components=6,
                  n_best=3, logs=''):
         self.n_clusters = n_clusters
         self.random_state = random_state
@@ -18,24 +19,43 @@ class Biclustering2d(BaseEstimator, TransformerMixin):
             self.logs = logs
         else:
             self.logs = os.getcwd()
-        self.biclustModel = SpectralBiclustering(n_clusters=self.n_clusters, random_state=self.random_state,
-                                                 method=self.scale, n_components=self.n_components,
-                                                 n_best=self.n_best)
 
-    def fit(self, X, y=None):
+    def fit(self, X, y):
         # Biclustering of the mean 2d image of all samples
-        X_mean = np.squeeze(np.mean(X, axis=0))
+        HC = X[y==0,:]
+        print(HC.shape)
+        X_mean = np.squeeze(np.mean(HC, axis=0))
+        #X_mean = np.squeeze(np.mean(X, axis=0))
+        self.biclustModel = self.create_model()
         self.biclustModel.fit(X_mean)
+
+        # Plotting the clustered matrix
+        fit_data = X_mean[np.argsort(self.biclustModel.row_labels_)]
+        fit_data = fit_data[:, np.argsort(self.biclustModel.column_labels_)]
+        plt.matshow(fit_data)
+        plt.title(self.n_clusters)
+        plt.show()
+
         return self
 
     def transform(self, X):
         X_reordered = np.empty(X.shape)
-        for i in range(len(X[0])):
+        for i in range(X.shape[0]):
             x = np.squeeze(X[i,:,:])
             x_clust = x[np.argsort(self.biclustModel.row_labels_)]
             x_clust = x_clust[:, np.argsort(self.biclustModel.column_labels_)]
             X_reordered[i, :, :] = x_clust
         return X_reordered
+
+    def create_model(self):
+
+        biclustModel = SpectralBiclustering(n_clusters=self.n_clusters, random_state=self.random_state,
+                                            method=self.scale, n_components=self.n_components,
+                                            n_best=self.n_best)
+
+
+
+        return biclustModel
 
     # ToDo: add these functions again
     # def set_params(self, **params):
@@ -63,4 +83,3 @@ class Biclustering2d(BaseEstimator, TransformerMixin):
 #     X_new = bcm.transform(X)
 #     plt.matshow(np.squeeze(X_new[0]), cmap=plt.cm.Blues)
 #     plt.show()
-
