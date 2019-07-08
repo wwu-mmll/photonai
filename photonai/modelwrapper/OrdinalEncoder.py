eb2ff70de94434ccf8dda93e78b8dddd0b301558
Ramona Leenings <leenings@uni-muenster.de>
2019-07-08 15:26:21 +0200

    add FeatureEncoder add Tests for Neuro adapt copy mechanism

---
+++ photonai/modelwrapper/OrdinalEncoder.py
@@ -0,0 +1,37 @@
+from sklearn.base import BaseEstimator
+from sklearn.preprocessing import OrdinalEncoder
+from ..base.PhotonBase import PipelineStacking
+import numpy as np
+
+
+class FeatureEncoder(BaseEstimator):
+
+    def __init__(self):
+        self.encoder_list = []
+
+    def fit(self, X, y=None, **kwargs):
+        # iterate X and if its a string column: encode and save encoder
+        self.encoder_list = list()
+        for i in range(X.shape[1]):
+            feature = X[:, i]
+            if isinstance(feature[0], str):
+                new_encoder = OrdinalEncoder()
+                feature = np.reshape(feature, (-1, 1))
+                new_encoder.fit(feature)
+                self.encoder_list.append(new_encoder)
+            else:
+                self.encoder_list.append(None)
+
+    def transform(self, X, y=None, **kwargs):
+        # iterate X and apply encoder if necessary
+        new_X = None
+        for i in range(X.shape[1]):
+            feature = X[:, i]
+            transformer = self.encoder_list[i]
+            if transformer is not None:
+                feature = np.reshape(feature, (-1, 1))
+                trans_X = transformer.transform(feature)
+            else:
+                trans_X = feature
+            new_X = PipelineStacking.stack_data(new_X, trans_X)
+        return new_X
