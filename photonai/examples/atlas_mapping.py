2a93cf4dcf4d9806dcbfcb701cbe36d26d8d1e55
Nils <nils.r.winter@gmail.com>
2019-07-16 17:39:41 +0200

    Add example for atlas mapper

--- photonai/examples/atlas_mapping.py
+++ photonai/examples/atlas_mapping.py
@@ -1,4 +1,3 @@
-
 from photonai.base.PhotonBase import Hyperpipe, PipelineElement, OutputSettings, PreprocessingPipe
 from photonai.optimization.Hyperparameters import FloatRange, Categorical, IntegerRange
 from photonai.neuro.AtlasMapping import AtlasMapper
@@ -6,39 +5,58 @@ from sklearn.model_selection import KFold
 import time
 import os
 import pandas as pd
+import glob
+import numpy as np
+
+
+#load nifti files
+file_path_list = glob.glob('/spm-data/Scratch/spielwiese_vincent/PAC2019/TestRun_PreProcessing/mri/mwp1age*.nii')
+print(file_path_list)
+X = sorted(file_path_list)
+print(X)
+
+#load labels
+PAClabels = pd.DataFrame(pd.read_excel(r'/spm-data/Scratch/spielwiese_ramona/PAC2019/old_files/PAC2019_data.xlsx'))
+PAClabels = PAClabels[['id', 'age']]
+PACIDs = [z.split("/mri/mwp1")[1].split("_")[0] for z in file_path_list]
+SelectedLabels = PAClabels[PAClabels['id'].isin(PACIDs)]
+y = SelectedLabels['age'].to_numpy()
+print(np.isnan(y))
+print(type(y))
+
 
 # YOU CAN SAVE THE TRAINING AND TEST RESULTS AND ALL THE PERFORMANCES IN THE MONGODB
 mongo_settings = OutputSettings(save_predictions='best')
-
 
 
 # DESIGN YOUR PIPELINE
 my_pipe = Hyperpipe('basic_svm_pipe',  # the name of your pipeline
                     optimizer='grid_search',  # which optimizer PHOTON shall use
                     metrics=['mean_absolute_error'],  # the performance metrics of your interest
-                    best_config_metric='mean_absolute_error',  # after hyperparameter search, the metric declares the winner config
-                    outer_cv=KFold(n_splits=3),  # repeat hyperparameter search three times
-                    inner_cv=KFold(n_splits=5),  # test each configuration ten times respectively,
+                    best_config_metric='mean_absolute_error',
+                    inner_cv=KFold(n_splits=2),  # test each configuration ten times respectively,
                     verbosity=1,
                     output_settings=mongo_settings)  # get error, warn and info message
 
 preprocessing = PreprocessingPipe()
-preprocessing += PipelineElement('BrainAtlas', atlas_name="AAL", extract_mode='vec')
+preprocessing += PipelineElement('BrainAtlas', atlas_name="AAL", extract_mode='vec', rois=['Amygdala_L', 'Amygdala_R'])
 my_pipe += preprocessing
-my_pipe += PipelineElement('SVR', hyperparameters={'kernel': Categorical(['rbf', 'linear']),
-                                                   'C': FloatRange(0.5, 2)}, gamma='scale')
+my_pipe += PipelineElement('SVR', hyperparameters={}, kernel='linear')
 
 
 # NOW TRAIN YOUR PIPELINE
 start_time = time.time()
 atlas_mapper = AtlasMapper()
-my_folder = ''
+my_folder = '/spm-data/Scratch/spielwiese_nils_winter/atlas_mapper_test'
 atlas_mapper.generate_mappings(my_pipe, my_folder)
 atlas_mapper.fit(X, y)
 elapsed_time = time.time() - start_time
 print(time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
 
+atlas_mapper = AtlasMapper()
+atlas_mapper.load_from_file('/spm-data/Scratch/spielwiese_nils_winter/atlas_mapper_test/basic_svm_pipe_atlas_mapper_meta.json')
 
+print(atlas_mapper.predict(X))
 debug = True
 
 
