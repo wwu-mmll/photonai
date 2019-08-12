9a016325f14aa27f6e336036129a4d2934c5d5eb
Nils <nils.r.winter@gmail.com>
2019-08-12 18:16:00 +0200

    Reorder example files

--- photonai/examples/basic/classification_pipeline.py
+++ photonai/examples/basic/classification_pipeline.py
@@ -1,5 +1,5 @@
 
-from photonai.base.PhotonBase import Hyperpipe, PipelineElement, OutputSettings, PreprocessingPipe
+from photonai.base.PhotonBase import Hyperpipe, PipelineElement, PreprocessingPipe
 from photonai.optimization.Hyperparameters import FloatRange, Categorical, IntegerRange
 from photonai.optimization.SpeedHacks import MinimumPerformance, DummyPerformance
 from photonai.investigator.Investigator import Investigator
@@ -44,27 +44,14 @@ my_pipe += PipelineElement('PCA', hyperparameters={'n_components': IntegerRange(
 # engage and optimize the good old SVM for Classification
 my_pipe += PipelineElement('SVC', hyperparameters={'kernel': Categorical(['rbf', 'linear']),
                                                    'C': FloatRange(0.5, 2)}, gamma='scale')
-#
-# my_pipe += PipelineElement('Graph', hyperparameters={'centrality__nr_nodes': IntegerRange(1, 3),
-#                                                      'centrality__huaihdsiud': Categorical(['linear', 'rbf']),
-#                                                      'xy__dhsud': FloatRange(0.5, 2)},
-#                            measures=["centrality", "xy", "a", "b", "c"])
-
 # my_pipe += PipelineElement('LogisticRegression', hyperparameters={'penalty': ['l1', 'l2'], 'C': [0.5, 1]})
 
-start_time = time.time()
+
 # NOW TRAIN YOUR PIPELINE
+start_time = time.time()
 my_pipe.fit(X, y)
 elapsed_time = time.time() - start_time
 print(time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
-# AND SHOW THE RESULTS IN THE WEBBASED PHOTON INVESTIGATOR TOOL
-# Investigator.show(my_pipe)
-# Investigator.load_from_file(my_pipe.name, my_pipe.output_settings.local_file)
-
-
-# YOU CAN ALSO SAVE THE BEST PERFORMING PIPELINE FOR FURTHER USE
-# my_pipe.save_optimum_pipe('optimum_pipe.photon')
-
 
 debug = True
 
