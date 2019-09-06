assuring result_tree entails all the expected calculation of metrics and correct mean and std on each tree level

photonai/processing/inner_folds.py
@@ -429,13 +429,14 @@ class InnerFoldManager(object):
                 except:
                     warnings.warn('No probabilities available.')
 
-            # if isinstance(y_pred, list):
-            #     y_pred = np.array(y_pred)
-            #     y_true = np.array(y_true)
+            if not isinstance(y_pred, list):
+                y_pred = np.asarray(y_pred).tolist()
+            if not isinstance(y_true, list):
+                y_true = np.asarray(y_true).tolist()
 
             score_result_object = MDBScoreInformation(metrics=output_metrics,
                                                       score_duration=final_scoring_time,
-                                                      y_pred=y_pred.tolist(), y_true=y_true.tolist(),
+                                                      y_pred=y_pred, y_true=y_true,
                                                       indices=np.asarray(indices).tolist(),
                                                       probabilities=probabilities)
             if save_feature_importances:

photonai/test/processing/results_tests.py
@@ -1,7 +1,10 @@
 import unittest
 import numpy as np
+import pandas as pd
+import os
 
 from sklearn.model_selection import KFold
+from sklearn.metrics import mean_absolute_error
 from sklearn.base import BaseEstimator, ClassifierMixin
 from photonai.base import Hyperpipe, PipelineElement, OutputSettings
 
@@ -10,42 +13,53 @@ class XPredictor(BaseEstimator, ClassifierMixin):
 
     _estimator_type = 'classifier'
 
-    def __init__(self):
+    def __init__(self, change_predictions = False):
         self.needs_y = False
         self.needs_covariates = False
+        self.change_predictions = change_predictions
         pass
 
     def fit(self, X, y=None, **kwargs):
         return self
 
     def predict(self, X, **kwargs):
+        if self.change_predictions:
+            # change it relative to value so that it is fold-specific
+            return XPredictor.adapt_X(X)
         return X
+
+    @staticmethod
+    def adapt_X(X):
+        return [i-(0.1*i) for i in X]
 
     def predict_proba(self, X):
         return X/10
 
 
 # Todo: build dummy estimator predicting X , register it, do hyperpipe analysis, reassure output=input
-class ResultHandlerTests(unittest.TestCase):
+class ResultHandlerAndHelperTests(unittest.TestCase):
 
-    @classmethod
-    def setUp(cls):
-        cls.y_true = np.linspace(1, 100, 100)
-        X = cls.y_true
-        cls.inner_fold_nr = 10
-        cls.outer_fold_nr = 5
+    def setUp(self):
 
-        cls.hyperpipe = Hyperpipe('test_prediction_collection',
-                                  inner_cv=KFold(n_splits=cls.inner_fold_nr),
-                                  outer_cv=KFold(n_splits=cls.outer_fold_nr),
-                                  metrics=['mean_absolute_error', 'mean_squared_error'],
-                                  best_config_metric='mean_absolute_error',
-                                  output_settings=OutputSettings(save_predictions='all'))
+        self.inner_fold_nr = 10
+        self.outer_fold_nr = 5
+        
+        self.y_true = np.linspace(1, 100, 100)
+        self.X = self.y_true
+        
+        self.hyperpipe = Hyperpipe('test_prediction_collection',
+                                   inner_cv=KFold(n_splits=self.inner_fold_nr),
+                                   outer_cv=KFold(n_splits=self.outer_fold_nr),
+                                   metrics=['mean_absolute_error', 'mean_squared_error'],
+                                   best_config_metric='mean_absolute_error',
+                                   output_settings=OutputSettings(save_predictions='all',
+                                                                  project_folder='./tmp'))
 
-        cls.hyperpipe += PipelineElement('PhotonTestXPredictor')
-        cls.hyperpipe.fit(X, cls.y_true)
+    def test_get_predictions(self):
 
-    def test_get_inner_val_preds(self):
+        self.hyperpipe += PipelineElement('PhotonTestXPredictor')
+        self.hyperpipe.fit(self.X, self.y_true)
+
         inner_preds_received = self.hyperpipe.results_handler.get_validation_predictions()
         first_outer_fold_info = next(iter(self.hyperpipe.cross_validation.outer_folds.values()))
         values_to_expect = np.asarray(first_outer_fold_info.train_indices) + 1.0
@@ -53,21 +67,74 @@ class ResultHandlerTests(unittest.TestCase):
         self.assertTrue(np.array_equal(inner_preds_received['y_true'], values_to_expect))
         self.assertTrue(np.array_equal(inner_preds_received['probabilities'], values_to_expect / 10))
 
-
-    def test_get_outer_val_preds(self):
         outer_preds_received = self.hyperpipe.results_handler.get_test_predictions()
         self.assertTrue(np.array_equal(outer_preds_received['y_pred'], self.y_true))
         self.assertTrue(np.array_equal(outer_preds_received['y_true'], self.y_true))
         self.assertTrue(np.array_equal(outer_preds_received['probabilities'], self.y_true/10))
 
-    def test_write_predictions(self):
-        pass
+        csv_file = pd.read_csv(os.path.join(self.hyperpipe.output_settings.results_folder, 'best_config_predictions.csv'))
+        self.assertTrue(np.array_equal(csv_file.y_pred.values, self.y_true))
+        self.assertTrue(np.array_equal(csv_file.y_true.values, self.y_true))
+        self.assertTrue(np.array_equal(csv_file.probabilities.values, self.y_true / 10))
+    
+    def test_metrics_and_aggregations(self):
+        
+        self.hyperpipe += PipelineElement('PhotonTestXPredictor', change_predictions=True)
+        X = np.linspace(0, 99, 100)
+        y_true = X
+        self.hyperpipe.fit(X, y_true)
 
+        def check_metrics(metric_name, expected_metric_list, mean_metrics):
+            for metric in mean_metrics:
+                if metric.metric_name == metric_name:
+                    if metric.operation == 'FoldOperations.MEAN':
+                        expected_val_mean = np.mean(expected_metric_list)
+                        self.assertEqual(expected_val_mean, metric.value)
+                    elif metric.operation == 'FoldOperations.STD':
+                        expected_val_std = np.std(expected_metric_list)
+                        self.assertAlmostEqual(expected_val_std, metric.value)
+            return expected_val_mean, expected_val_std
 
-class MDBHelperTests(unittest.TestCase):
+        outer_collection = {'train': list(), 'test': list()}
+        for i, (_, outer_fold) in enumerate(self.hyperpipe.cross_validation.outer_folds.items()):
+            outer_fold_results = self.hyperpipe.results.outer_folds[i]
+            config = outer_fold_results.tested_config_list[0]
+            inner_fold_results = config.inner_folds
 
-    def test_aggregate_metrics_inner_folds(self):
-        pass
+            inner_fold_metrics = {'train': list(), 'test': list()}
+            for _, inner_fold in self.hyperpipe.cross_validation.inner_folds[outer_fold.fold_id].items():
+                tree_result = inner_fold_results[inner_fold.fold_nr - 1]
 
-    def test_aggregate_metrics_outer_folds(self):
-        pass
+                test_indices_to_check = outer_fold.train_indices[inner_fold.test_indices]
+                expected_test_mae = mean_absolute_error(XPredictor.adapt_X(test_indices_to_check), test_indices_to_check)
+                inner_fold_metrics['test'].append(expected_test_mae)
+                self.assertEqual(expected_test_mae, tree_result.validation.metrics['mean_absolute_error'])
+
+                train_indices_to_check = outer_fold.train_indices[inner_fold.train_indices]
+                expected_train_mae = mean_absolute_error(XPredictor.adapt_X(train_indices_to_check), train_indices_to_check)
+                inner_fold_metrics['train'].append(expected_train_mae)
+                self.assertEqual(expected_train_mae, tree_result.training.metrics['mean_absolute_error'])
+
+                # get expected train and test mean and std respectively and calculate mean and std again.
+
+            check_metrics('mean_absolute_error', inner_fold_metrics['train'], config.metrics_train)
+            check_metrics('mean_absolute_error', inner_fold_metrics['test'], config.metrics_test)
+
+            expected_outer_test_mae = mean_absolute_error(XPredictor.adapt_X(outer_fold.test_indices), outer_fold.test_indices)
+            outer_collection['test'].append(expected_outer_test_mae)
+            self.assertEqual(outer_fold_results.best_config.best_config_score.validation.metrics['mean_absolute_error'], expected_outer_test_mae)
+
+            expected_outer_train_mae = mean_absolute_error(XPredictor.adapt_X(outer_fold.train_indices), outer_fold.train_indices)
+            outer_collection['train'].append(expected_outer_train_mae)
+            self.assertEqual(outer_fold_results.best_config.best_config_score.training.metrics['mean_absolute_error'], expected_outer_train_mae)
+
+        # check again in overall best config attribute
+        check_metrics('mean_absolute_error', outer_collection['train'],
+                      self.hyperpipe.results.metrics_train)
+        check_metrics('mean_absolute_error', outer_collection['test'],
+                      self.hyperpipe.results.metrics_test)
+
+        # check if those agree with helper function output
+        outer_fold_performances = self.hyperpipe.results_handler.get_performance_outer_folds()
+        self.assertListEqual(outer_fold_performances['mean_absolute_error'], outer_collection['test'])
+