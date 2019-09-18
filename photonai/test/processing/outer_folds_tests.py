a12e4326c16e4ea7500dce12edd1b7e5bd9b728d
Ramona Leenings <leenings@uni-muenster.de>
2019-09-18 09:46:32 +0200

    method stubs

--- photonai/test/processing/outer_folds_tests.py
+++ photonai/test/processing/outer_folds_tests.py
@@ -6,6 +6,11 @@ class OuterFoldTests(unittest.TestCase):
     def setUp(self):
         pass
 
+    def test_prepare_optimization(self):
+        pass
 
-    def prepare_optimization(self):
+    def test_fit(self):
+        pass
+
+    def test_fit_dummy(self):
         pass
