2b63429460e9973d01ab01e8ec736cad0d9cf7a6
Ramona Leenings <leenings@uni-muenster.de>
2020-01-17 14:56:17 +0100

    manuscript changes

--- photonai/examples/basic/switch.py
+++ photonai/examples/basic/switch.py
@@ -2,6 +2,7 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import KFold
 
 from photonai.base import Hyperpipe, PipelineElement, Switch, OutputSettings
+from photonai.optimization import IntegerRange
 
 # GET DATA
 X, y = load_breast_cancer(True)
@@ -9,7 +10,7 @@ X, y = load_breast_cancer(True)
 # CREATE HYPERPIPE
 my_pipe = Hyperpipe('basic_switch_pipe',
                     optimizer='random_grid_search',
-                    optimizer_params={'n_configurations': 10},
+                    optimizer_params={'n_configurations': 15},
                     metrics=['accuracy', 'precision', 'recall'],
                     best_config_metric='accuracy',
                     outer_cv=KFold(n_splits=3),
@@ -23,8 +24,14 @@ my_pipe += Switch('TransformerSwitch',
                    PipelineElement('PCA', test_disabled=True)])
 
 # Estimator Switch
-svm = PipelineElement('SVC')
-tree = PipelineElement('DecisionTreeClassifier')
+svm = PipelineElement('SVC',
+                      hyperparameters={'kernel': ['rbf', 'linear']})
+
+tree = PipelineElement('DecisionTreeClassifier',
+                       hyperparameters={'min_samples_split': IntegerRange(2, 5),
+                                        'min_samples_leaf': IntegerRange(1, 5),
+                                        'criterion': ['gini', 'entropy']})
+
 my_pipe += Switch('EstimatorSwitch', [svm, tree])
 
 my_pipe.fit(X, y)
