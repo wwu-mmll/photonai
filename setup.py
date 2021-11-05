ca7100f8d98d96f92281328c39fb3f48ee8db7cb
Nils Winter <nils.r.winter@gmail.com>
2021-11-05 15:51:32 +0100

    Upgrade to PHOTONAI version 2.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from setuptools import setup, find_packages
 
 
-__version__ = '2.1.0'
+__version__ = '2.2.0'
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
