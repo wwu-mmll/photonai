95306c724c190c791f2b3371f3ade12f5af50937
Jan Ernsting <j.ernsting@uni-muenster.de>
2022-11-14 23:10:04 +0100

    Added missing pbr dependency

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup
+from setuptools import setup
 
 setup(
     pbr=True
