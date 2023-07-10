7993008b10577511e20a72a2b89a900ca5a67e53
Ramona Leenings <leenings@uni-muenster.de>
2023-07-10 18:55:29 +0200

    restructure the model zoo and write unit tests

--- examples/basic/classification.py
+++ examples/basic/classification.py
@@ -1,8 +1,12 @@
-from photonai import ClassificationPipe
+from photonai import ClassificationPipe, ClassifierSwitch
 from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import ShuffleSplit
+
 
 X, y = load_breast_cancer(return_X_y=True)
-
-my_pipe = ClassificationPipe('breast_cancer_analysis')
-my_pipe.set_default_pipeline()
+my_pipe = ClassificationPipe(name='breast_cancer_analysis',
+                             inner_cv=ShuffleSplit(n_splits=2),
+                             dim_reduction=True,
+                             n_pca_components=10,
+                             use_test_set=True)
 my_pipe.fit(X, y)
--- photonai/base/model_zoo.py
+++ photonai/base/model_zoo.py
@@ -1,11 +1,14 @@
+import logging
 from photonai.base.hyperpipe import Hyperpipe, OutputSettings
-from photonai.base.photon_elements import PipelineElement, Switch
+from photonai.base.photon_elements import PipelineElement, Switch, Stack
+from photonai.optimization.hyperparameters import IntegerRange
 from photonai.optimization import FloatRange
 from sklearn.model_selection import KFold, StratifiedKFold
 from sklearn.base import BaseEstimator, ClassifierMixin, RegressorMixin
 from sklearn.model_selection._split import BaseCrossValidator, BaseShuffleSplit, _RepeatedSplits
 from photonai.photonlogger.logger import logger
 from typing import Optional, List, Union
+from pathlib import Path
 import numpy as np
 import pandas as pd
 
@@ -43,15 +46,51 @@ class RegressorSwitch(Switch, RegressorMixin):
         super(RegressorSwitch, self).__init__(name, elements=element_list)
 
 
-class DefaultPipelineGenerator:
+class DefaultPipeline(Hyperpipe):
 
-    def set_default_pipeline(self,
-                             scaling: bool = True,
-                             imputation: bool = False,
-                             imputation_nan_value=np.nan,
-                             dim_reduction: bool = False,
-                             n_pca_components: Union[int, float, None] = None,
-                             add_estimator: bool = True):
+    def __init__(self,
+                 name: str = 'default_pipeline',
+                 project_folder: Union[str, Path] = './',
+                 inner_cv: Union[BaseCrossValidator, BaseShuffleSplit, _RepeatedSplits] = KFold(n_splits=10),
+                 outer_cv: Union[BaseCrossValidator, BaseShuffleSplit, _RepeatedSplits] = KFold(n_splits=5),
+                 metrics: list = None,
+                 best_config_metric: str = '',
+                 optimizer: str = 'random_grid_search',
+                 optimizer_params: dict = {'n_configurations': 25},
+                 X_csv_path: Union[str, Path, None] = None,
+                 y_csv_path: Union[str, Path, None] = None,
+                 delimiter=",",
+                 add_default_pipeline_elements: bool = True,
+                 scaling: bool = True,
+                 imputation: bool = False,
+                 imputation_nan_value=np.nan,
+                 feature_selection: bool = False,
+                 dim_reduction: bool = False,
+                 n_pca_components: Union[int, float, None, IntegerRange, FloatRange] = None,
+                 add_estimator: bool = True,
+                 default_estimator: BaseEstimator = None,
+                 **kwargs):
+
+        self.X_csv_path = X_csv_path
+        self.y_csv_path = y_csv_path
+        self.delimiter = delimiter
+
+        super(DefaultPipeline, self).__init__(name=name,
+                                              project_folder=project_folder,
+                                              inner_cv=inner_cv,
+                                              outer_cv=outer_cv,
+                                              metrics=metrics,
+                                              best_config_metric=best_config_metric,
+                                              optimizer=optimizer,
+                                              optimizer_params=optimizer_params,
+                                              **kwargs)
+
+        if add_default_pipeline_elements is True:
+            self.set_default_pipeline(scaling, imputation, imputation_nan_value, feature_selection, dim_reduction,
+                                      n_pca_components, add_estimator, default_estimator)
+
+    def set_default_pipeline(self, scaling, imputation, imputation_nan_value, feature_selection, dim_reduction,
+                             n_pca_components, add_estimator, default_estimator):
         logger.stars()
         if scaling is True:
             logger.photon_system_log("USING STANDARD SCALER ")
@@ -59,103 +98,94 @@ class DefaultPipelineGenerator:
         if imputation is True:
             logger.photon_system_log("USING SIMPLE IMPUTER ")
             self.add(PipelineElement('SimpleImputer', missing_values=imputation_nan_value))
+        if feature_selection:
+            self.add(PipelineElement('FClassifSelectPercentile',
+                                     hyperparameters={'percentile': IntegerRange(10, 50, step=10)},
+                                     test_disabled=True))
         if dim_reduction is True:
             logger.photon_system_log("USING PCA ")
-            self.add(PipelineElement('PCA', n_components=n_pca_components))
+            self.add(PipelineElement('PCA', n_components=n_pca_components, test_disabled=True))
         if add_estimator is True:
-            if not hasattr(self, 'default_estimator_cls'):
-                raise ValueError("Must have estimator_cls attribute set to an estimator class")
-
-            self += self.default_estimator_cls()
+            if default_estimator is None:
+                raise ValueError("If paramater add_estimator is True, then default_estimator cannot be None. Use a "
+                                 "string keyword argument such as 'SVC' or 'SVR' or photon native constructs "
+                                 "such as PipelineElements, Switchs oder Stacks")
+            ve = ValueError("Currently only string keywords such as 'SVC' or 'SVR' or photon native constructs "
+                                 "such as PipelineElements, Switchs oder Stacks are allowed")
+            if isinstance(default_estimator, str):
+                estimator = PipelineElement(default_estimator)
+            else:
+                try:
+                    estimator = default_estimator()
+                    if not isinstance(estimator, (PipelineElement, Switch, Stack, ClassifierSwitch, RegressorSwitch)):
+                        raise ve
+                except Exception as e:
+                    logging.error(e)
+                    raise ve
+            self += estimator
             logger.photon_system_log("USING " + self.elements[-1].name.upper())
-            for idx, e in enumerate(self.elements[-1].elements):
-                logger.photon_system_log(e.name)
-                logger.photon_system_log(e.initial_hyperparameters)
-                logger.photon_system_log("---")
+            if hasattr(self.elements[-1], 'elements'):
+                for idx, e in enumerate(self.elements[-1].elements):
+                    logger.photon_system_log(e.name)
+                    logger.photon_system_log(e.initial_hyperparameters)
+                    logger.photon_system_log("---")
         logger.stars()
 
+    def fit(self, X=None, y=None):
+        if (X is not None and self.X_csv_path is not None) or (y is not None and self.y_csv_path is not None):
+            raise ValueError("You can either give the fit function data or the pipe definition paths "
+                             "to csv files to load data from. Not both.")
 
-class ClassificationPipe(Hyperpipe, DefaultPipelineGenerator):
+        if (X is None and self.X_csv_path is None) or (y is None and self.y_csv_path is None):
+            raise ValueError("Please give either the fit function array-like X and y data "
+                             "or give the pipe definition paths to csv files to load X and y data from.")
+
+        X = X if X is not None else pd.read_csv(self.X_csv_path, delimiter=self.delimiter)
+        y = y if y is not None else pd.read_csv(self.y_csv_path, delimiter=self.delimiter)
+        super().fit(X, y)
+
+
+class ClassificationPipe(DefaultPipeline):
 
     def __init__(self,
-                 name: Optional[str],
-                 inner_cv: Union[BaseCrossValidator, BaseShuffleSplit, _RepeatedSplits] = KFold(n_splits=10),
-                 outer_cv: Union[BaseCrossValidator, BaseShuffleSplit, _RepeatedSplits] = KFold(n_splits=5),
-                 optimizer: str = 'grid_search',
-                 optimizer_params: dict = {},
-                 metrics: list = ['balanced_accuracy', 'specificity', 'precision', 'recall', 'f1_score'],
+                 name: Optional[str] = 'classification_pipeline',
+                 project_folder: Union[str, Path] = './classification/',
+                 metrics: list = None,
                  best_config_metric: str = 'balanced_accuracy',
-                 eval_final_performance: bool = None,
-                 use_test_set: bool = True,
-                 test_size: float = 0.2,
-                 project_folder: str = '',
-                 calculate_metrics_per_fold: bool = True,
-                 calculate_metrics_across_folds: bool = False,
-                 ignore_sanity_checks: bool = False,
-                 random_seed: int = None,
-                 verbosity: int = 0,
-                 learning_curves: bool = False,
-                 learning_curves_cut: FloatRange = None,
-                 output_settings: OutputSettings = None,
-                 performance_constraints: list = None,
-                 permutation_id: str = None,
-                 cache_folder: str = None,
-                 nr_of_processes: int = 1,
-                 multi_threading: bool = True,
-                 allow_multidim_targets: bool = False,
-                 default_estimator_cls: Union[BaseEstimator, ClassifierSwitch] = ClassifierSwitch):
+                 default_estimator: Union[BaseEstimator, ClassifierSwitch] = ClassifierSwitch,
+                 **kwargs):
 
-        self.default_estimator_cls = default_estimator_cls
+        metrics = metrics if metrics is not None else ['balanced_accuracy',
+                                                       'specificity',
+                                                       'precision',
+                                                       'recall',
+                                                       'f1_score']
 
-        super(ClassificationPipe, self).__init__(name, inner_cv, outer_cv, optimizer, optimizer_params,
-                                                 metrics, best_config_metric,
-                                                 eval_final_performance, use_test_set, test_size,
-                                                 project_folder, calculate_metrics_per_fold,
-                                                 calculate_metrics_across_folds,
-                                                 ignore_sanity_checks, random_seed, verbosity, learning_curves,
-                                                 learning_curves_cut, output_settings, performance_constraints,
-                                                 permutation_id, cache_folder, nr_of_processes, multi_threading,
-                                                 allow_multidim_targets)
+        super(ClassificationPipe, self).__init__(name=name,
+                                                 project_folder=project_folder,
+                                                 metrics=metrics,
+                                                 best_config_metric=best_config_metric,
+                                                 default_estimator=default_estimator,
+                                                 **kwargs)
 
 
-class RegressionPipe(Hyperpipe, DefaultPipelineGenerator):
+class RegressionPipe(DefaultPipeline):
 
     def __init__(self,
-                 name: Optional[str],
-                 inner_cv: Union[BaseCrossValidator, BaseShuffleSplit, _RepeatedSplits] = KFold(n_splits=10),
-                 outer_cv: Union[BaseCrossValidator, BaseShuffleSplit, _RepeatedSplits] = KFold(n_splits=5),
-                 optimizer: str = 'grid_search',
-                 optimizer_params: dict = {},
-                 metrics: list = ['mean_absolute_error', 'mean_squared_error'],
+                 name: Optional[str] = 'classification_pipeline',
+                 project_folder: Union[str, Path] = './classification/',
+                 metrics: list = None,
                  best_config_metric: str = 'mean_squared_error',
-                 eval_final_performance: bool = None,
-                 use_test_set: bool = True,
-                 test_size: float = 0.2,
-                 project_folder: str = '',
-                 calculate_metrics_per_fold: bool = True,
-                 calculate_metrics_across_folds: bool = False,
-                 ignore_sanity_checks: bool = False,
-                 random_seed: int = None,
-                 verbosity: int = 0,
-                 learning_curves: bool = False,
-                 learning_curves_cut: FloatRange = None,
-                 output_settings: OutputSettings = None,
-                 performance_constraints: list = None,
-                 permutation_id: str = None,
-                 cache_folder: str = None,
-                 nr_of_processes: int = 1,
-                 multi_threading: bool = True,
-                 allow_multidim_targets: bool = False,
-                 default_estimator_cls: Union[BaseEstimator, RegressorSwitch] = RegressorSwitch):
+                 default_estimator: Union[BaseEstimator, RegressorSwitch] = RegressorSwitch,
+                 **kwargs):
 
-        self.default_estimator_cls = default_estimator_cls
+        metrics = metrics if metrics is not None else ['mean_absolute_error',
+                                                       'mean_squared_error',
+                                                       'explained_variance']
 
-        super(RegressionPipe, self).__init__(name, inner_cv, outer_cv, optimizer, optimizer_params,
-                                             metrics, best_config_metric,
-                                             eval_final_performance, use_test_set, test_size,
-                                             project_folder, calculate_metrics_per_fold,
-                                             calculate_metrics_across_folds,
-                                             ignore_sanity_checks, random_seed, verbosity, learning_curves,
-                                             learning_curves_cut, output_settings, performance_constraints,
-                                             permutation_id, cache_folder, nr_of_processes, multi_threading,
-                                             allow_multidim_targets)
+        super(RegressionPipe, self).__init__(name=name,
+                                             project_folder=project_folder,
+                                             metrics=metrics,
+                                             best_config_metric=best_config_metric,
+                                             default_estimator=default_estimator,
+                                             **kwargs)
