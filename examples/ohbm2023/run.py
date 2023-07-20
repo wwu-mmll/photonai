abc0181fd2676f4258897c4419ba61e7fccbcff5
Jan Ernsting <j.ernsting@uni-muenster.de>
2023-07-20 16:51:38 -0400

    Added summary csv generation

--- examples/ohbm2023/run.py
+++ examples/ohbm2023/run.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from run_elements import *
 from config_selectors import DefaultConfigSelector, RandomConfigSelector
 from collect_results import ResultCollector
@@ -8,40 +10,61 @@ import os
 list_of_config_selectors = {'default': DefaultConfigSelector,
                             'random': RandomConfigSelector}
 
-config_selector_name = 'random'
 multiprocessing = False
+calculate = False
 
-list_of_dataset_runners = {'breast_cancer': BreastCancerRunner,}
-                           # 'diabetes': DiabetesRunner,
+list_of_dataset_runners = {'breast_cancer': BreastCancerRunner,
+                           'diabetes': DiabetesRunner}
                            # 'abalone': AbaloneRunner,
                            # 'habermans_survival': HabermansSurvivalRunner,
                            # 'autistic': AutisticRunner,
                            # 'parkinson': ParkinsonsRunner}
 
-current_config_selector = list_of_config_selectors[config_selector_name]
-
 procs = []
+base_project_folder = './tmp/'
+for config_selector_name, current_config_selector in list_of_config_selectors.items():
+    for name, runner_type in list_of_dataset_runners.items():
 
-for name, runner_type in list_of_dataset_runners.items():
+        project_folder = base_project_folder + config_selector_name
+        os.makedirs(project_folder, exist_ok=True)
+        runner = runner_type(name=name,
+                             project_folder=project_folder,
+                             best_config_selector=current_config_selector())
+        func = runner.run_analysis
 
-    project_folder = './tmp/' + config_selector_name
-    os.makedirs(project_folder, exist_ok=True)
-    runner = runner_type(name=name,
-                         project_folder=project_folder,
-                         best_config_selector=current_config_selector())
-    func = runner.run_analysis
+        if not multiprocessing and calculate:
+            func()
+        elif multiprocessing and calculate:
+            proc = Process(target=func)
+            procs.append(proc)
+            proc.start()
+        else:
+            print(f"Calculate is {calculate}")
 
-    if multiprocessing is False:
-        # func()
-        pass
+    if multiprocessing is True:
+        for proc in procs:
+            proc.join()
+
+    collector = ResultCollector(project_folder)
+    collector.collect_results()
+
+regression_results = None
+classification_results = None
+
+for config_selector_name, _ in list_of_config_selectors.items():
+    project_folder = base_project_folder + config_selector_name
+    classification = pd.read_csv(project_folder + '/classification.csv')
+    classification['config_selector_name'] = config_selector_name
+    if classification_results is None:
+        classification_results = classification
     else:
-        proc = Process(target=func)
-        procs.append(proc)
-        proc.start()
+        classification_results = pd.concat([classification_results, classification], ignore_index=True)
+    regression = pd.read_csv(project_folder + '/regression.csv')
+    regression['config_selector_name'] = config_selector_name
+    if regression_results is None:
+        regression_results = regression
+    else:
+        regression_results = pd.concat([regression_results, regression], ignore_index=True)
 
-if multiprocessing is True:
-    for proc in procs:
-        proc.join()
-
-collector = ResultCollector(project_folder)
-collector.collect_results()
+regression_results.to_csv(base_project_folder+"/regression.csv")
+classification_results.to_csv(base_project_folder+"/classification.csv")
