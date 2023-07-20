08977d026319b1b2291aa7cfef1d9430ccaabf8a
Ramona Leenings <leenings@uni-muenster.de>
2023-07-20 15:26:00 -0400

    check if folders exist

--- examples/ohbm2023/run.py
+++ examples/ohbm2023/run.py
@@ -1,19 +1,19 @@
 from run_elements import *
-from config_selectors import DefaultConfigSelector
+from config_selectors import DefaultConfigSelector, RandomConfigSelector
 from collect_results import ResultCollector
 from multiprocessing import Process
 import os
 
 
-list_of_dataset_runners = {'breast_cancer': BreastCancerRunner,
-                           'diabetes': DiabetesRunner, 
-                           'abalone': AbaloneRunner,
-                           'habermans_survival': HabermansSurvivalRunner,
-                           'autistic': AutisticRunner,
-                           'parkinson': ParkinsonsRunner}
+list_of_dataset_runners = {'breast_cancer': BreastCancerRunner,}
+                           # 'diabetes': DiabetesRunner,
+                           # 'abalone': AbaloneRunner,
+                           # 'habermans_survival': HabermansSurvivalRunner,
+                           # 'autistic': AutisticRunner,
+                           # 'parkinson': ParkinsonsRunner}
 
-current_config_selector = DefaultConfigSelector
-config_selector_name = 'default'
+current_config_selector = RandomConfigSelector
+config_selector_name = 'random'
 procs = []
 
 for name, runner_type in list_of_dataset_runners.items():
@@ -22,7 +22,7 @@ for name, runner_type in list_of_dataset_runners.items():
     os.makedirs(project_folder, exist_ok=True)
     runner = runner_type(name=name,
                          project_folder=project_folder,
-                         best_config_selector=DefaultConfigSelector())
+                         best_config_selector=current_config_selector())
 
     func = runner.run_analysis
     func()
